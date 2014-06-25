bf8872851bc16e3785a740e7ada57badfa8aa8eb
Max Krummenacher <max.krummenacher@toradex.com>
2014-06-25 09:51:06 +0200

    timestamp-service: change to new timestamp format
    Patch pending in meta-angstrom

---
+++ recipes-core/systemd/timestamp-service/load-timestamp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Set the system clock from hardware clock
+# If the timestamp is 1 day or more recent than the current time,
+# use the timestamp instead.
+SYSTEMDATE=$(/bin/date -u "+%4Y%2m%2d%2H%2M")
+
+TIMESTAMP=$(/bin/cat /etc/timestamp 2>/dev/null)
+
+if [ ${TIMESTAMP} -gt $SYSTEMDATE ] 2>/dev/null ; then
+	echo "Update systemtime from /etc/timestamp"
+	/bin/date -u ${TIMESTAMP#????}${TIMESTAMP%????????}
+fi
+
+if [ "$1" = "--save" ] ; then
+	/bin/date -u +%4Y%2m%2d%2H%2M > /etc/timestamp
+fi
+
