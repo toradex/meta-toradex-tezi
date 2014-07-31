2b3ad86229ce333ddb671187b1050010e297236d
Max Krummenacher <max.krummenacher@toradex.com>
2014-07-31 14:11:52 +0200

    usb-rndis: drop use of ifconfig in favor of ip
    ifconfig does no longer understand 192.168.11.2/24 and its
    use is deprecated, so instead of changing to another way of
    stating the subnetmask use /sbin/ip.

--- recipes-core/usb-rndis/files/start-rndis.sh
+++ recipes-core/usb-rndis/files/start-rndis.sh
@@ -2,8 +2,8 @@
 # configures the usb gadget to provide rnidis, 
 case "$1" in
 	start)
-		/sbin/modprobe g_ether dev_addr=00:14:2d:ff:ff:ff host_addr=00:14:2d:ff:ff:fe \
-		  sleep 1 && /sbin/ifconfig usb0 192.168.11.2 \
+		/sbin/modprobe g_ether dev_addr=00:14:2d:ff:ff:ff host_addr=00:14:2d:ff:ff:fe ; \
+		  sleep 1 && /sbin/ip address add 192.168.11.2/24 dev usb0; /sbin/ip link set usb0 up \
 		  && /usr/sbin/udhcpd -S /etc/udhcpd-usb-rndis.conf
 	;;
 	
