67e8b1fa79536f74a69c453079417bcc26982fc3
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2015-10-20 16:47:09 +0200

    apalis/colibri_t20/30 aka tegras: fix usb rndis operation
    We noticed that our latest move to using systemd/networkd leaves the
    rndis0 interface down. Manually bringing the link up solved the issue.
    As a workaround add upping the link to our start-rndis.sh script.

--- recipes-core/usb-rndis/files/tegra/start-rndis.sh
+++ recipes-core/usb-rndis/files/tegra/start-rndis.sh
@@ -5,9 +5,11 @@ case "$1" in
 		echo 0 > /sys/class/android_usb/android0/enable
 		echo rndis > /sys/class/android_usb/android0/functions
 		echo 1 > /sys/class/android_usb/android0/enable
+		ip link set rndis0 up
 	;;
 	
 	stop)
+		ip link set rndis0 down
 		echo 0 > /sys/class/android_usb/android0/enable
 	;;
 esac
