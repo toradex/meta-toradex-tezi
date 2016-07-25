1bb4bbfa115ff49b9eb8738122d5f4a2bcc78d95
Stefan Agner <stefan.agner@toradex.com>
2016-07-25 16:05:18 -0700

    create boot parameter to choose whether autoinstall is allowed
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- recipes-core/u-boot-distro-boot/files/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttymxc0,115200 quiet init=/init video=mxcfb0:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb1:dev=lcd,EDT-VGA,if=RGB24 video=mxcfb2:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb2:off video=mxcfb3:off rootfstype=squashfs root=/dev/ram
+setenv bootargs console=ttymxc0,115200 quiet init=/init video=mxcfb0:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb1:dev=lcd,EDT-VGA,if=RGB24 video=mxcfb2:off video=mxcfb3:off rootfstype=squashfs root=/dev/ram autoinstall
 load ${devtype} ${devnum}:${bootpart} ${kernel_addr_r} ${prefix}zImage
 load ${devtype} ${devnum}:${bootpart} ${fdt_addr_r} ${prefix}${fdt_file}
 load ${devtype} ${devnum}:${bootpart} ${ramdisk_addr_r} ${prefix}tezi-apalis-imx6.squashfs
