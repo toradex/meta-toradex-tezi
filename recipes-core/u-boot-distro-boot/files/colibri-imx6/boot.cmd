7f83c4a09f0c4d9f718dc24e9610ee3081a87647
Stefan Agner <stefan.agner@toradex.com>
2017-01-30 13:53:04 -0800

    u-boot-distro-boot: add boot scripts for Colibri iMX6
    The Colibri iMX6 boot scripts use slightly different video kernel
    parameters.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

---
+++ recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd
@@ -0,0 +1,5 @@
+setenv bootargs console=ttymxc0,115200 quiet init=/init video=mxcfb0:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb1:dev=lcd,EDT-VGA,if=RGB666 rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}
+
+load ${devtype} ${devnum}:${bootpart} ${ramdisk_addr_r} ${prefix}tezi.itb
+
+bootm ${ramdisk_addr_r}
