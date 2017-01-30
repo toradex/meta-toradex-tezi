adb752caff3cd19a41ae6520397aaf404ef9562c
Stefan Agner <stefan.agner@toradex.com>
2017-01-30 14:33:03 -0800

    scripts: add Colibri iMX6 support
    Add FIT image configuration for Colibri iMX6 and extend the script.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -6,11 +6,14 @@
 # image build
 #
 
+MACHINE=apalis-imx6
+#MACHINE=colibri-imx6
+DSTDIR=Tezi_Apalis_iMX6_V0.6/
+#DSTDIR=Tezi_Colibri_iMX6_V0.6/
 SRCDIR=../../../layers/meta-toradex-tezi/scripts/
-DSTDIR=Tezi_Apalis_iMX6_V0.5/
 
 mkdir ${DSTDIR}
-cp ${SRCDIR}/image.json ${DSTDIR}
+cp ${SRCDIR}/image-${MACHINE}.json ${DSTDIR}/image.json
 cp ${SRCDIR}/wrapup.sh ${DSTDIR}
 cp ${SRCDIR}/tezi.png ${DSTDIR}
 cp -r ${SRCDIR}/recovery/ ${DSTDIR}
@@ -21,5 +24,5 @@ cp u-boot.img ${DSTDIR}
 
 cp boot.scr ${DSTDIR}
 cp boot-sdp.scr ${DSTDIR}
-cp ${SRCDIR}/tezi.its ./
+cp ${SRCDIR}/tezi-${MACHINE}.its ./tezi.its
 mkimage -f ./tezi.its ${DSTDIR}/tezi.itb
