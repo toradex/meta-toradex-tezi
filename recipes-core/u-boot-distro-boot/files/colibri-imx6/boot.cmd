c945e1545494d39927246b3a6fdde7d7eb7565da
Stefan Agner <stefan.agner@toradex.com>
2017-05-24 19:59:11 -0700

    do not overwrite init path
    Since commit 0531e57c4215 ("tezi-init: create initrd specific init")
    we make use of the different init paths when using a initramfs (for
    development) vs a squashfs (for deployment). Not explicitly running
    /init will use directly /sbin/init, which does not mount devtmpfs
    which gets automatically mounted by the kernel in the non-initramfs
    case. This fixes:
      mount: mounting devtmpfs on /dev/ failed: Device or resource busy
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttymxc0,115200 quiet init=/init video=mxcfb0:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb1:dev=lcd,EDT-VGA,if=RGB666 rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}
+setenv bootargs console=ttymxc0,115200 quiet video=mxcfb0:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb1:dev=lcd,EDT-VGA,if=RGB666 rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}
 
 load ${devtype} ${devnum}:${bootpart} ${ramdisk_addr_r} ${prefix}tezi.itb
 
--- recipes-core/u-boot-distro-boot/files/colibri-imx7/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx7/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttymxc0,115200 quiet init=/init rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}
+setenv bootargs console=ttymxc0,115200 quiet rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}
 
 load ${devtype} ${devnum}:${bootpart} ${ramdisk_addr_r} ${prefix}tezi.itb
 
