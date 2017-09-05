b1ebb8abb74e22e465f454371a95010f4dcb300d
Stefan Agner <stefan.agner@toradex.com>
2017-09-05 14:39:33 -0700

    u-boot-distro-boot: enable fdt relocation in distroboot scripts
    By default the U-Boot shipped with the Toradex BSP disables device
    tree relocation by setting fdt_high to 0xffffffff. This is harmful
    when using a FIT image, since U-Boot will expand the device tree
    in-place, which will overwrite the start of the ramdisk directly
    adjacent in the device tree inside the FIT image...
    
    Enable relocation by clearing fdt_high.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd
@@ -1,5 +1,10 @@
 setenv bootargs console=ttymxc0,115200 quiet video=mxcfb0:dev=hdmi,640x480M@60,if=RGB24 video=mxcfb1:dev=lcd,EDT-VGA,if=RGB666 rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}
 
+# Reenable fdt relocation since in place fdt edits corrupt the ramdisk
+# in a FIT image...
+setenv fdt_high
+
+# Load FIT image from location as detected by distroboot
 load ${devtype} ${devnum}:${bootpart} ${ramdisk_addr_r} ${prefix}tezi.itb
 
 bootm ${ramdisk_addr_r}
