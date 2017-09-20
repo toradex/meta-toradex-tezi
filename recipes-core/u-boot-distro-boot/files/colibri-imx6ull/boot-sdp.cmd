aee7f6c700dcadc3afa9c93604c9ec6b8c409b2b
Stefan Agner <stefan.agner@toradex.com>
2017-09-20 15:42:07 -0700

    u-boot-distro-boot: add boot scripts for colibri-imx6ull
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

---
+++ recipes-core/u-boot-distro-boot/files/colibri-imx6ull/boot-sdp.cmd
@@ -0,0 +1,4 @@
+setenv bootargs console=ttymxc0,115200 quiet rootfstype=squashfs root=/dev/ram autoinstall clk_ignore_unused
+
+# Execute previously loaded FIT image
+bootm ${ramdisk_addr_r}
---
+++ recipes-core/u-boot-distro-boot/files/colibri-imx6ull/boot.cmd
@@ -0,0 +1,10 @@
+setenv bootargs console=ttymxc0,115200 quiet rootfstype=squashfs root=/dev/ram autoinstall clk_ignore_unused ${teziargs}
+
+# Reenable fdt relocation since in place fdt edits corrupt the ramdisk
+# in a FIT image...
+setenv fdt_high
+
+# Load FIT image from location as detected by distroboot
+load ${devtype} ${devnum}:${bootpart} ${ramdisk_addr_r} ${prefix}tezi.itb
+
+bootm ${ramdisk_addr_r}
