dd06e39b600063b0e036c5f5d35ab1f046444e2b
Stefan Agner <stefan.agner@toradex.com>
2018-07-31 12:11:57 +0200

    u-boot-distro-boot: use hardcoded addresses for SDP
    The SDP configuration uses a hardcoded address where it loads the
    FIT image to. Use the same hard coded address in the SDP boot script.
    This makes the SDP based download independent of U-Boot address
    configurations.
    
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- recipes-core/u-boot-distro-boot/files/colibri-imx7/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx7/boot-sdp.cmd
@@ -1,4 +1,4 @@
 setenv bootargs console=ttymxc0,115200 quiet rootfstype=squashfs root=/dev/ram autoinstall
 
 # Execute previously loaded FIT image, choose configuration depending on runtime SoC detection
-bootm ${ramdisk_addr_r}#config@${soc}${variant}
+bootm 0x82100000#config@${soc}${variant}
