d4bdc189c419a0eb4fbd79e3606f567047554c28
Gerard Salvatella <gerard.salvatella@toradex.com>
2019-05-22 13:48:24 +0200

    u-boot-distro-boot: apalis-imx8: add boot scripts
    add boot scripts for both recovery and flash modes.
    
    Signed-off-by: Gerard Salvatella <gerard.salvatella@toradex.com>

---
+++ recipes-core/u-boot-distro-boot/files/apalis-imx8/boot-sdp.cmd
@@ -0,0 +1,9 @@
+setenv bootargs 'console=ttyLP1,115200 earlycon=lpuart32,0x5a070000,115200,115200 video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}' 
+
+# Note: the following memory locations are bound to the ones used to generate
+# the boot container image with im8_mkimage.
+
+# Load hdmi firwmware
+hdp load 0x82fe0000
+# Execute previously loaded FIT image
+bootm 0x83000000
---
+++ recipes-core/u-boot-distro-boot/files/apalis-imx8/boot.cmd
@@ -0,0 +1,20 @@
+setenv bootargs 'console=ttyLP1,115200 earlycon=lpuart32,0x5a070000,115200,115200 video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}' 
+
+# Set address outside the range where FIT Image is extracted
+setenv ramdisk_addr_r 0x94400000 
+
+# Reenable fdt relocation since in place fdt edits corrupt the ramdisk
+# in a FIT image...
+setenv fdt_high
+
+# Load hdmi firmware
+run loadhdp; hdp load ${hdp_addr}
+
+# Load FIT Image and boot
+fatload mmc ${mmcdev}:${mmcpart} ${ramdisk_addr_r} tezi.itb
+bootm ${ramdisk_addr_r}
+
+# TODO: modify when distroboot is set by default
+# Load FIT image from location as detected by distroboot 
+# load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${prefix}tezi.itb
+
