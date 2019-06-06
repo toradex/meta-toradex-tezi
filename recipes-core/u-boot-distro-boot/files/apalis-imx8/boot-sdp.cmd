3175b89bdd131079097cc4682dac2c4debdc355d
Gerard Salvatella <gerard.salvatella@toradex.com>
2019-06-06 11:54:38 +0200

    u-boot-distro-boot: apalis-imx8: disable vpu driver
    Kernel crashes due to a VPU driver issue after a patch modifying the
    hdmi clock frequency limits. Since tezi doesn't need VPU, disabling the
    driver solves the issue.
    
    Signed-off-by: Gerard Salvatella <gerard.salvatella@toradex.com>

--- recipes-core/u-boot-distro-boot/files/apalis-imx8/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/apalis-imx8/boot-sdp.cmd
@@ -1,4 +1,4 @@
-setenv bootargs "console=ttyLP1,115200 earlycon=lpuart32,0x5a070000,115200,115200 video=HDMI-A-1:640x480 video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}" 
+setenv bootargs "console=ttyLP1,115200 earlycon=lpuart32,0x5a070000,115200,115200 video=HDMI-A-1:640x480 video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off initcall_blacklist=vpu_driver_init rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}"
 
 # Note: the following memory locations are bound to the ones used to generate
 # the boot container image with im8_mkimage.
