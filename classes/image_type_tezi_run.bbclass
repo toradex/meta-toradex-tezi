toradex/meta-toradex-tezi

commit 5a1706aca01f77654657f6b20d535cfa84b3be07

tezi-run-metadata: install meta data to a subdirectory of DEPLOY_DIR
Since the tezi-run-metadata recipe is deploying some files like
wrapup.sh that is also being deployed by other recipes like
tezi-metadata, to avoid file conflict in DEPLOY_DIR, we change to
install metadata to a subdirectory ${DEPLOY_DIR}/tezi-run-metadata.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

classes/image_type_tezi_run.bbclass
@@ -190,7 +190,7 @@ build_deploytar () {
 	fi
 
 	mkdir ${IMAGE_NAME}/
-	cp -L -R ${SPL_BINARY} ${TEZI_UBOOT_BINARIES} tezi.itb wrapup.sh ${TEZI_IMAGE_FILES} tezi.png ${TEZI_DISTRO_BOOT_SCRIPTS} recovery-linux.sh recovery-windows.bat recovery/ ${IMAGE_NAME}/
+	cp -L -R ${SPL_BINARY} ${TEZI_UBOOT_BINARIES} ${TEZI_IMAGE_FILES} ${TEZI_DISTRO_BOOT_SCRIPTS} tezi.itb tezi-run-metadata/* ${IMAGE_NAME}/
 
 	# zip does update if the file exist, explicitly delete before adding files to the archive
 	if [ -e ${IMAGE_NAME}.zip ]; then

recipes-bsp/tezi-run-metadata/tezi-run-metadata_0.2.bb
@@ -31,66 +31,58 @@ SRC_URI_append_apalis-tk1-mainline = " \
 SRC_URI[md5sum] = "11cf6d9b4b18b7f35f06ed91bfc0b3a8"
 SRC_URI[sha256sum] = "064bff2e4cb4a0c0f8bceeaf6dd2cef1e682869e0b22d49e40bc6a8326ec14c9"
 
+TEZI_RUN_DEPLOYDIR = "${DEPLOYDIR}/${BPN}"
+
 inherit deploy
 
-do_deploy () {
-    install -m 644 ${WORKDIR}/wrapup.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.png ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.its ${DEPLOYDIR}
+deploy_common () {
+    install -d ${TEZI_RUN_DEPLOYDIR}/recovery
+    install -m 644 ${WORKDIR}/wrapup.sh ${TEZI_RUN_DEPLOYDIR}
+    install -m 644 ${WORKDIR}/tezi.png ${TEZI_RUN_DEPLOYDIR}
+    install -m 755 ${WORKDIR}/recovery-linux.sh ${TEZI_RUN_DEPLOYDIR}
+    install -m 644 ${WORKDIR}/recovery-windows.bat ${TEZI_RUN_DEPLOYDIR}
 
-    install -m 755 ${WORKDIR}/recovery-linux.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/recovery-windows.bat ${DEPLOYDIR}
-    install -d ${DEPLOYDIR}/recovery
-    install -m 644 ${WORKDIR}/recovery/imx_usb.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx6_usb_rom.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx6_usb_sdp_spl.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx6_usb_sdp_uboot.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx7_usb_rom.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx7_usb_sdp_uboot.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx6ull_usb_rom.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${WORKDIR}/recovery/mx6ull_usb_sdp_uboot.conf ${DEPLOYDIR}/recovery/
-    install -m 644 ${S}/README.imx_usb ${DEPLOYDIR}/recovery/README
-    install -m 755 ${S}/imx_usb ${DEPLOYDIR}/recovery/
-    install -m 644 ${S}/imx_usb.exe ${DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/tezi.its ${DEPLOYDIR}
+}
+
+do_deploy () {
+    deploy_common
+
+    install -m 644 ${WORKDIR}/recovery/imx_usb.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx6_usb_rom.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx6_usb_sdp_spl.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx6_usb_sdp_uboot.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx7_usb_rom.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx7_usb_sdp_uboot.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx6ull_usb_rom.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/mx6ull_usb_sdp_uboot.conf ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${S}/README.imx_usb ${TEZI_RUN_DEPLOYDIR}/recovery/README
+    install -m 755 ${S}/imx_usb ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${S}/imx_usb.exe ${TEZI_RUN_DEPLOYDIR}/recovery/
 }
 
 do_deploy_apalis-t30-mainline () {
-    install -m 644 ${WORKDIR}/wrapup.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.png ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.its ${DEPLOYDIR}
+    deploy_common
 
-    install -m 755 ${WORKDIR}/recovery-linux.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/recovery-windows.bat ${DEPLOYDIR}
-    install -d ${DEPLOYDIR}/recovery
-    install -m 644 ${WORKDIR}/recovery/Apalis_T30_2GB_800Mhz.bct ${DEPLOYDIR}/recovery/
-    install -m 644 ${S}/README.tegrarcm ${DEPLOYDIR}/recovery/README
-    install -m 755 ${S}/tegrarcm ${DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/Apalis_T30_2GB_800Mhz.bct ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${S}/README.tegrarcm ${TEZI_RUN_DEPLOYDIR}/recovery/README
+    install -m 755 ${S}/tegrarcm ${TEZI_RUN_DEPLOYDIR}/recovery/
 }
 
 do_deploy_apalis-tk1 () {
-    install -m 644 ${WORKDIR}/wrapup.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.png ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.its ${DEPLOYDIR}
+    deploy_common
 
-    install -m 755 ${WORKDIR}/recovery-linux.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/recovery-windows.bat ${DEPLOYDIR}
-    install -d ${DEPLOYDIR}/recovery
-    install -m 644 ${WORKDIR}/recovery/PM375_Hynix_2GB_H5TC4G63AFR_RDA_924MHz.bct ${DEPLOYDIR}/recovery/
-    install -m 644 ${S}/README.tegrarcm ${DEPLOYDIR}/recovery/README
-    install -m 755 ${S}/tegrarcm ${DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/PM375_Hynix_2GB_H5TC4G63AFR_RDA_924MHz.bct ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${S}/README.tegrarcm ${TEZI_RUN_DEPLOYDIR}/recovery/README
+    install -m 755 ${S}/tegrarcm ${TEZI_RUN_DEPLOYDIR}/recovery/
 }
 
 do_deploy_apalis-tk1-mainline () {
-    install -m 644 ${WORKDIR}/wrapup.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.png ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/tezi.its ${DEPLOYDIR}
+    deploy_common
 
-    install -m 755 ${WORKDIR}/recovery-linux.sh ${DEPLOYDIR}
-    install -m 644 ${WORKDIR}/recovery-windows.bat ${DEPLOYDIR}
-    install -d ${DEPLOYDIR}/recovery
-    install -m 644 ${WORKDIR}/recovery/PM375_Hynix_2GB_H5TC4G63AFR_RDA_924MHz.bct ${DEPLOYDIR}/recovery/
-    install -m 644 ${S}/README.tegrarcm ${DEPLOYDIR}/recovery/README
-    install -m 755 ${S}/tegrarcm ${DEPLOYDIR}/recovery/
+    install -m 644 ${WORKDIR}/recovery/PM375_Hynix_2GB_H5TC4G63AFR_RDA_924MHz.bct ${TEZI_RUN_DEPLOYDIR}/recovery/
+    install -m 644 ${S}/README.tegrarcm ${TEZI_RUN_DEPLOYDIR}/recovery/README
+    install -m 755 ${S}/tegrarcm ${TEZI_RUN_DEPLOYDIR}/recovery/
 }
 
 addtask deploy before do_package after do_install

commit 9244125089a6e346018feb1cfb6543fd7e99f66f

image_type_tezi_run.bbclass: fix a regression
A regression was introduced by commit 5a1706ac:
[ tezi-run-metadata: install meta data to a subdirectory of DEPLOY_DIR ]

in which, the tezi.png, wrapup.sh were installed into
${DEPLOY_DIR}/tezi-run-metadata instead of ${DEPLOY_DIR}. Hence we need
check if those files exist in ${DEPLOY_DIR}/tezi-run-metadata instead.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

classes/image_type_tezi_run.bbclass
@@ -117,9 +117,9 @@ def rootfs_tezi_run_create_json(d, flash_type, type_specific_name = False):
     data["description"] = d.getVar('DESCRIPTION')
     data["version"] = d.getVar('TDX_VER_PACKAGE_MIN')
     data["release_date"] = d.getVar('TDX_VERDATE')[1:9]
-    if os.path.exists(os.path.join(deploydir, "wrapup.sh")):
+    if os.path.exists(os.path.join(deploydir, "tezi-run-metadata", "wrapup.sh")):
         data["wrapup_script"] = "wrapup.sh"
-    if os.path.exists(os.path.join(deploydir, "tezi.png")):
+    if os.path.exists(os.path.join(deploydir, "tezi-run-metadata", "tezi.png")):
         data["icon"] = "tezi.png"
     data["isinstaller"] = True
 