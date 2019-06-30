toradex/meta-toradex-tezi

commit 34e8d189110d83046a846c23a07b3506b4301fde

meta: fix some regressions
Some regressions were introduced by commits 6a14155e:
[ meta: move imx-boot related code to imx-boot recipe ]

the regressions are as follows:
- recovery image should not be built in imx-boot, since it requires
  tezi.itb which is generated by image command build_fitimage.
- it should be deploy_mx8_append rather than deploy_append_mx8 or else
  it would not append any code to deploy_mx8 function.
- MACHINE_BOOT_FILES could be None for some machines, so we should
  handle this case in image_type_tezi_run.bbclass.

Fixed by:
- add a build_recovery_image prefunc run after build_fitimage.
- introduce SOC_FAMILY variable to control mx8 common code, this allows
  the build_recovery_image function to be expanded easier.
- introduce a imx-boot-common bbclass to include all the imx-boot
  related varaibles.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

classes/image_type_tezi_run.bbclass
@@ -26,7 +26,7 @@ def rootfs_tezi_run_emmc(d):
     offset_spl = d.getVar('OFFSET_SPL_PAYLOAD')
 
     bootpart_rawfiles = []
-    bootpart_filelist = [ "boot.scr", "tezi.itb" ] + d.getVar('MACHINE_BOOT_FILES').split()
+    bootpart_filelist = [ "boot.scr", "tezi.itb" ] + (d.getVar('MACHINE_BOOT_FILES') or "").split()
     has_spl = d.getVar('SPL_BINARY')
     if has_spl:
         bootpart_rawfiles.append(
@@ -178,6 +178,26 @@ build_fitimage () {
 	mkimage -f ${DEPLOY_DIR_IMAGE}/tezi.its ${DEPLOY_DIR_IMAGE}/tezi.itb
 }
 
+build_recovery_image () {
+	if [ "${SOC_FAMILY}" = "mx8" ]; then
+		${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${TOOLS_NAME} -commit > head.hash
+		cat ${DEPLOY_DIR_IMAGE}/u-boot.bin head.hash > u-boot-hash.bin
+		cp ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${ATF_MACHINE_NAME} u-boot-atf.bin
+		dd if=u-boot-hash.bin of=u-boot-atf.bin bs=1K seek=128
+
+		${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${TOOLS_NAME} -soc QM -rev B0 \
+			-append ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${SECO_FIRMWARE_NAME} \
+			-c -scfw ${DEPLOY_DIR_IMAGE}/${BOOT_TOOLS}/${SC_FIRMWARE_NAME} \
+			-ap u-boot-atf.bin a53 0x80000000 \
+			-ap ${DEPLOY_DIR_IMAGE}/boot-sdp.scr a53 0x82e00000 \
+			-ap ${DEPLOY_DIR_IMAGE}/hdmitxfw.bin a53 0x82fe0000 \
+			-ap ${DEPLOY_DIR_IMAGE}/tezi.itb a53 0x83000000 \
+			-out ${DEPLOY_DIR_IMAGE}/recovery.bin
+	fi
+}
+build_recovery_image[dirs] = "${WORKDIR}/recovery"
+build_recovery_image[cleandirs] = "${WORKDIR}/recovery"
+
 build_deploytar () {
 	cd ${DEPLOY_DIR_IMAGE}
 
@@ -189,7 +209,7 @@ build_deploytar () {
 	mkdir ${TDX_VER_ID}
 	cp -L -R ${SPL_BINARY} ${TEZI_UBOOT_BINARIES} ${TEZI_IMAGE_FILES} ${TEZI_DISTRO_BOOT_SCRIPTS} tezi.itb tezi-run-metadata/* ${TDX_VER_ID}
 
-	if [ -n ${MACHINE_BOOT_FILES} ]; then
+	if [ -n "${MACHINE_BOOT_FILES}" ]; then
 	    cp ${MACHINE_BOOT_FILES} ${TDX_VER_ID}
 	fi
 
@@ -204,6 +224,7 @@ build_deploytar () {
 
 python do_assemble_fitimage() {
     bb.build.exec_func('build_fitimage', d)
+    bb.build.exec_func('build_recovery_image', d)
     bb.build.exec_func('rootfs_tezirun_run_json', d)
     bb.build.exec_func('build_deploytar', d)
 }

classes/imx-boot-common.bbclass
@@ -1,0 +1,54 @@
+# IMX boot common variables
+
+IMX_EXTRA_FIRMWARE      = "firmware-imx-8 imx-sc-firmware"
+IMX_EXTRA_FIRMWARE_mx8m = "firmware-imx-8m"
+IMX_EXTRA_FIRMWARE_mx8x = "firmware-imx-8x imx-sc-firmware"
+
+BOOT_NAME = "imx-boot"
+
+SC_FIRMWARE_NAME ?= "scfw_tcm.bin"
+SECO_FIRMWARE_NAME ?= "mx8qm-ahab-container.img"
+
+ATF_MACHINE_NAME ?= "bl31-imx8qm.bin"
+ATF_MACHINE_NAME_mx8qm = "bl31-imx8qm.bin"
+ATF_MACHINE_NAME_mx8qxp = "bl31-imx8qxp.bin"
+ATF_MACHINE_NAME_mx8mq = "bl31-imx8mq.bin"
+ATF_MACHINE_NAME_mx8mm = "bl31-imx8mm.bin"
+ATF_MACHINE_NAME_append = "${@bb.utils.contains('COMBINED_FEATURES', 'optee', '-optee', '', d)}"
+
+DCD_NAME ?= "imx8qm_dcd.cfg.tmp"
+DCD_NAME_mx8qm = "imx8qm_dcd.cfg.tmp"
+DCD_NAME_mx8qxp = "imx8qx_dcd.cfg.tmp"
+
+UBOOT_NAME = "u-boot-${MACHINE}.bin-${UBOOT_CONFIG}"
+BOOT_CONFIG_MACHINE = "${BOOT_NAME}-${MACHINE}-${UBOOT_CONFIG}.bin"
+
+BOOT_TOOLS ?= "imx-boot-tools"
+TOOLS_NAME ?= "mkimage_imx8"
+
+SOC_TARGET       ?= "INVALID"
+SOC_TARGET_mx8qm  = "iMX8QM"
+SOC_TARGET_mx8qxp = "iMX8QX"
+SOC_TARGET_mx8mq  = "iMX8M"
+SOC_TARGET_mx8mm  = "iMX8MM"
+
+IMXBOOT_TARGETS ?= \
+    "${@bb.utils.contains('UBOOT_CONFIG', 'fspi', 'flash_flexspi', \
+        bb.utils.contains('UBOOT_CONFIG', 'nand', 'flash_nand', \
+                                                  'flash flash_dcd', d), d)}"
+IMXBOOT_TARGETS_mx8qxp = \
+    "${@bb.utils.contains('UBOOT_CONFIG', 'fspi', 'flash_flexspi', \
+        bb.utils.contains('UBOOT_CONFIG', 'nand', 'flash_nand', \
+                                                  'flash', d), d)}"
+IMXBOOT_TARGETS_mx8qxpa0 = \
+    "${@bb.utils.contains('UBOOT_CONFIG', 'fspi', 'flash_flexspi_a0', \
+        bb.utils.contains('UBOOT_CONFIG', 'nand', 'flash_nand_a0', \
+                                                  'flash_a0 flash_dcd_a0', d), d)}"
+
+BOOT_STAGING       = "${S}/${SOC_TARGET}"
+BOOT_STAGING_mx8mm = "${S}/iMX8M"
+
+SOC_FAMILY      = "INVALID"
+SOC_FAMILY_mx8  = "mx8"
+SOC_FAMILY_mx8m = "mx8m"
+SOC_FAMILY_mx8x = "mx8x"

recipes-bsp/imx-mkimage/imx-boot_%.bbappend
@@ -1,13 +1,9 @@
-deploy_append_mx8() {
-    ${BOOT_STAGING}/${TOOLS_NAME} -commit > head.hash
+deploy_mx8_append () {
+    ${DEPLOYDIR}/imx-boot-tools/${TOOLS_NAME} -commit > head.hash
     cat ${BOOT_STAGING}/u-boot.bin head.hash > u-boot-hash.bin
     cp ${BOOT_STAGING}/bl31.bin u-boot-atf.bin
     dd if=u-boot-hash.bin of=u-boot-atf.bin bs=1K seek=128
 
-    ${BOOT_STAGING}/${TOOLS_NAME} -soc QM -rev B0 -append ${BOOT_STAGING}/mx8qm-ahab-container.img \
-        -c -scfw ${BOOT_STAGING}/scfw_tcm.bin -ap u-boot-atf.bin a53 0x80000000 -ap boot-sdp.scr a53 0x82e00000 \
-        -ap ${DEPLOY_DIR_IMAGE}/hdmitxfw.bin a53 0x82fe0000 -ap tezi.itb a53 0x83000000 -out ${DEPLOYDIR}/recovery.bin
-
-    ${BOOT_STAGING}/${TOOLS_NAME} -soc QM -rev B0 -append ${BOOT_STAGING}/mx8qm-ahab-container.img \
+    ${DEPLOYDIR}/imx-boot-tools/${TOOLS_NAME} -soc QM -rev B0 -append ${BOOT_STAGING}/mx8qm-ahab-container.img \
         -c -scfw ${BOOT_STAGING}/scfw_tcm.bin -ap u-boot-atf.bin a53 0x80000000 -out ${DEPLOYDIR}/flash.bin
 }

commit 0dc197ee48f0c95bb35cb9f54eb5357203a00762

image_type_tezi_run.bbclass: inherit imx-boot-common
A regression was introduced by commit 34e8d189:
[ meta: fix some regressions ]

imx-boot-common.bbclass was introduced by this commit but it's not
being inherited by image class.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

classes/image_type_tezi_run.bbclass
@@ -15,6 +15,8 @@ TEZI_RUNIMG_DEPENDS ??= "virtual/bootloader:do_deploy u-boot-distro-boot:do_depl
                          ${@'%s:do_deploy' % d.getVar('IMAGE_BOOTLOADER') if d.getVar('IMAGE_BOOTLOADER') else ''} \
                         "
 
+inherit imx-boot-common
+
 def fitimg_get_size(d):
     import os
     return os.path.getsize(os.path.join(d.getVar('DEPLOY_DIR_IMAGE'), 'tezi.itb')) / (1024 * 1024)