69fc052cba4a6ab8493c7bec15167298265554af
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2019-07-12 11:41:33 +0200

    init: fix vnc vs. rdp text
    Fix VNC vs. RDP text being printed on console. While at it further clean this
    up and also fix a comment in boot-sdp.cmd.
    
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

--- recipes-core/u-boot-distro-boot/files/apalis-imx8/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/apalis-imx8/boot-sdp.cmd
@@ -1,7 +1,7 @@
 setenv bootargs "quiet console=ttyLP1,115200 video=HDMI-A-1:640x480 initcall_blacklist=vpu_driver_init rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}"
 
 # Note: the following memory locations are bound to the ones used to generate
-# the boot container image with im8_mkimage.
+# the boot container image with mkimage_imx8 (see image_type_tezi_run.bbclass).
 
 # Load hdmi firwmware
 hdp load 0x82fe0000
