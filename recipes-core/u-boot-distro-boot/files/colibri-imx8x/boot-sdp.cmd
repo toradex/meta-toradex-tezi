39515cd2ff2424adaad0926a9fdf5f0544c63a68
Ming Liu <liu.ming50@gmail.com>
2019-07-14 22:09:41 +0800

    u-boot-distro-boot: boot-sdp.cmd: set fdt_high environment variable
    We need set fdt_high environment variable since now tezi.itb is being
    added to recovery image as data rather than AP image.
    
    Without this change, I observed kernel stuck at "Starting kernel".
    
    Signed-off-by: Ming Liu <liu.ming50@gmail.com>

--- recipes-core/u-boot-distro-boot/files/colibri-imx8x/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx8x/boot-sdp.cmd
@@ -1,5 +1,9 @@
 setenv bootargs "quiet console=ttyLP3,115200 video=VGA-1:640x480 initcall_blacklist=vpu_driver_init rootfstype=squashfs root=/dev/ram autoinstall ${teziargs}"
 
+# Reenable fdt relocation since in place fdt edits corrupt the ramdisk
+# in a FIT image...
+setenv fdt_high
+
 # Note: the following memory locations are bound to the ones used to generate
 # the boot container image with im8_mkimage.
 
