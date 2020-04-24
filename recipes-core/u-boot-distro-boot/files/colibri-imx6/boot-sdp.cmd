e8d958094000ea6c5c97e53c0d8ad2f5edd0181b
Igor Opaniuk <igor.opaniuk@toradex.com>
2020-04-24 11:50:18 +0300

    u-boot-distro-boot: drop video depth in bootargs
    Drop video depth enforcing from bootargs, let KMS automatically detect
    proper mode settings for output.
    
    Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

--- recipes-core/u-boot-distro-boot/files/colibri-imx6/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx6/boot-sdp.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480-16@60D video=HDMI-A-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall
+setenv bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480M@60D video=HDMI-A-1:640x480M@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall
 
 # Execute previously loaded FIT image
 bootm 0x12100000
