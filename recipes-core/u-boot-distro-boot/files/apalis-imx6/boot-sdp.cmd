f295b6e334d506bd130bb65fd78e68fe1f933d08
Marcel Ziswiler <marcel.ziswiler@toradex.com>
2020-08-11 14:40:46 +0200

    apalis-imx6: re-enable analogue vga output
    Related-to: TEI-626
    Signed-off-by: Marcel Ziswiler <marcel.ziswiler@toradex.com>

--- recipes-core/u-boot-distro-boot/files/apalis-imx6/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/apalis-imx6/boot-sdp.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480D video=HDMI-A-1:640x480-16@60D video=LVDS-1:d video=VGA-1:d rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall
+setenv bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480D video=HDMI-A-1:640x480-16@60D video=LVDS-1:d video=VGA-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall
 
 # Execute previously loaded FIT image
 bootm 0x12100000
