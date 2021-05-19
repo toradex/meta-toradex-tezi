f173f04c9cf42e9bf48db5ca2f8bca10ed2f3afe
Denys Drozdov <denys.drozdov@toradex.com>
2021-05-19 18:14:56 +0300

    apalis-imx8: Update Easy Installer addresses
    Downstream u-boot defines ramdisk address at 0x8a000000
    and hdp firmware address @ 0x9c000000. Keep default
    ramdisk address in distro-boot and update uuu.auto script
    
    Signed-off-by: Denys Drozdov <denys.drozdov@toradex.com>

--- recipes-core/u-boot-distro-boot/files/apalis-imx8/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/apalis-imx8/boot.cmd
@@ -1,9 +1,9 @@
 # apalis-imx8 hardware setup
 
 env set bootcmd_hdp 'load ${devtype} ${devnum}:${distro_bootpart} ${hdp_addr} ${hdp_file}; hdp load ${hdp_addr}'
-env set ramdisk_addr_r 0x83000000
+env set ramdisk_addr_r 0x8a000000
+env set hdp_addr 0x9c000000
 env set vidargs 'video=LVDS-1:d video=HDMI-A-1:640x480-16@60D'
-
 env set bootcmd_args 'env set bootargs quiet ${vidargs} initcall_blacklist=vpu_driver_init rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall clk_ignore_unused pci=nomsi ${teziargs}'
 env set bootcmd_run 'bootm ${ramdisk_addr_r}#config@freescale_${fdtfile}'
 env set bootcmd 'run bootcmd_args && run bootcmd_hdp && run bootcmd_tezi && run bootcmd_run'
@@ -18,7 +18,7 @@ then
     # Recovery mode
     # Note: the memory locations are bound to the ones as used in uuu.auto
 
-    env set bootcmd_hdp 'hdp load 0x82fe0000'
+    env set bootcmd_hdp 'hdp load ${hdp_addr}'
     env set bootcmd_tezi ';'
 else
     env set bootcmd_tezi 'load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${tezi_image}'
