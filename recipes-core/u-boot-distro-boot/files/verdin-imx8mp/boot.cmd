6b864b673cbe6eb07458ee4be8937024998eaa14
Denys Drozdov <denys.drozdov@toradex.com>
2021-06-01 15:58:39 +0300

    u-boot-distro-boot: verdin-imx8mp: load overlays dynamically
    The distro-boot script has been changed to load
    required overlays  from /boot/overlays.txt
    
    Related-to: TEI-743
    Signed-off-by: Denys Drozdov <denys.drozdov@toradex.com>

--- recipes-core/u-boot-distro-boot/files/verdin-imx8mp/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/verdin-imx8mp/boot.cmd
@@ -6,7 +6,7 @@ env set ramdisk_addr_r 0x46400000
 env set vidargs 'video=HDMI-A-1:1280x720-16@60D video=HDMI-A-2:1280x720-16@60D'
 
 env set bootcmd_args 'env set bootargs quiet ${vidargs} initcall_blacklist=vpu_driver_init rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall clk_ignore_unused pci=nomsi ${teziargs}'
-env set bootcmd_run 'bootm ${ramdisk_addr_r}#config@freescale_${fdtfile}'
+
 env set bootcmd 'run bootcmd_args && run bootcmd_hdp && run bootcmd_tezi && run bootcmd_run'
 env set tezi_image ${prefix}tezi.itb
 
@@ -23,5 +23,43 @@ then
 else
     env set bootcmd_tezi 'load ${devtype} ${devnum}:${distro_bootpart} ${ramdisk_addr_r} ${tezi_image}'
 fi
+
+# Load user specified overlays from eMMC partition (file overlays.txt)
+
+test -n ${m4boot} || env set m4boot ';'
+test -n ${fdtfile} || env set fdtfile ${fdt_file}
+test -n ${boot_part} || env set boot_part ${distro_bootpart}
+test -n ${root_part} || env set root_part 2
+test -n ${boot_devnum} || env set boot_devnum ${devnum}
+test -n ${root_devnum} || env set root_devnum ${devnum}
+test -n ${kernel_image} || env set kernel_image Image.gz
+test -n ${boot_devtype} || env set boot_devtype ${devtype}
+test -n ${overlays_file} || env set overlays_file "overlays.txt"
+test -n ${overlays_prefix} || env set overlays_prefix "overlays/"
+
+# TODO: U-boot has to enable these variables fastboot protocol
+env set boot_devtype 	mmc
+env set boot_devnum	2
+env set boot_part	1
+
+test ${boot_devtype} = "mmc" && env set load_cmd 'load ${boot_devtype} ${boot_devnum}:${boot_part}'
+test ${boot_devtype} = "usb" && env set load_cmd 'load ${boot_devtype} ${boot_devnum}:${boot_part}'
+test ${boot_devtype} = "tftp" && env set load_cmd 'tftp'
+test ${boot_devtype} = "dhcp" && env set load_cmd 'dhcp'
+
+env set fdt_high
+env set fdt_resize true
+env set fitconf_fdt_overlays
+
+env set set_load_overlays_file 'env set load_overlays_file "${load_cmd} \\${loadaddr} \\${overlays_file}; env import -t \\${loadaddr} \\${filesize}"'
+env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#config@\\${overlay_file}\\"\"; env set overlay_file; done; true"'
+
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${ramdisk_addr_r}#config@freescale_\${fdtfile}\${fitconf_fdt_overlays}'
+
+run set_load_overlays_file
+run set_apply_overlays
+
+run load_overlays_file
+run apply_overlays
 
 run bootcmd
