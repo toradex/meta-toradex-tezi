7f56fb11759c84010363e557a64783da234cc99d
Philippe Schenker <philippe.schenker@toradex.com>
2021-11-10 17:58:46 +0100

    u-boot-distro-boot: boot-sdp.cmd: Ignore existing environment
    In recovery-mode we want to recover the modules hence we want to
    ignore as much of the possible existing stuff as possible.
    
    This commit makes shure, that the used environment is set to a
    known state which works and was compiled-into u-boot.
    
    Related-to: TEI-955
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

--- recipes-core/u-boot-distro-boot/files/apalis-imx6/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/apalis-imx6/boot-sdp.cmd
@@ -1,23 +1,9 @@
+# Start from a known environment
+# This is possible here, since apalis-imx6 has no variants that need dynamically set variables to boot.
+env default -a
+
 env set ramdisk_addr_r 0x12100000
 setenv bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480D video=HDMI-A-1:640x480-16@60D video=LVDS-1:d video=VGA-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall
-
-# Load user specified overlays from eMMC partition (file overlays.txt)
-
-test -n ${m4boot} || env set m4boot ';'
-test -n ${fdtfile} || env set fdtfile ${fdt_file}
-test -n ${boot_part} || env set boot_part ${distro_bootpart}
-test -n ${root_part} || env set root_part 2
-test -n ${boot_devnum} || env set boot_devnum ${devnum}
-test -n ${root_devnum} || env set root_devnum ${devnum}
-test -n ${kernel_image} || env set kernel_image Image.gz
-test -n ${boot_devtype} || env set boot_devtype ${devtype}
-test -n ${overlays_file} || env set overlays_file "overlays.txt"
-test -n ${overlays_prefix} || env set overlays_prefix "overlays/"
-
-test ${boot_devtype} = "mmc" && env set load_cmd 'load ${boot_devtype} ${boot_devnum}:${boot_part}'
-test ${boot_devtype} = "usb" && env set load_cmd 'load ${boot_devtype} ${boot_devnum}:${boot_part}'
-test ${boot_devtype} = "tftp" && env set load_cmd 'tftp'
-test ${boot_devtype} = "dhcp" && env set load_cmd 'dhcp'
 
 env set fdt_high
 env set fdt_resize true
