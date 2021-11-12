45bbe65ce36bb3a15a20f40b5001f78f472e4df4
Philippe Schenker <philippe.schenker@toradex.com>
2021-11-12 14:12:45 +0100

    u-boot-distro-boot: colibri-imx(7|6ull): use fixed eval-v3 device tree
    This device tree runs on all of our boards and is sufficient for the
    usage as a recovery-tool.
    
    Related-to: TEI-955
    Signed-off-by: Philippe Schenker <philippe.schenker@toradex.com>

--- recipes-core/u-boot-distro-boot/files/colibri-imx7/boot-sdp.cmd
+++ recipes-core/u-boot-distro-boot/files/colibri-imx7/boot-sdp.cmd
@@ -1,4 +1,5 @@
 setenv bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall
 
-# Execute previously loaded FIT image, choose configuration depending on runtime SoC detection
-bootm 0x82100000#config@${soc}-colibri${variant}-${fdt_board}.dtb
+# Execute previously loaded FIT image, choose configuration only from variables
+# which are not influenced by the environment
+bootm 0x82100000#config@${soc}-colibri${variant}-eval-v3.dtb
