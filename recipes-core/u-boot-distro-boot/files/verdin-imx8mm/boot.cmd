fa0dea1f60fbf86c7a0b670e1d8b1fb427c45850
Denys Drozdov <denys.drozdov@toradex.com>
2021-12-16 21:07:24 +0200

    configs: verdin-imx8m: allocate 64M for the linux kernel
    U-boot changes kernel memory map to allow load recent
    kernel up to 64M in memory. Change the ram disk
    disk address to 0x4740_0000 for both uuu and
    distro-boot script accordingly.
    
    Signed-off-by: Denys Drozdov <denys.drozdov@toradex.com>

--- recipes-core/u-boot-distro-boot/files/verdin-imx8mm/boot.cmd
+++ recipes-core/u-boot-distro-boot/files/verdin-imx8mm/boot.cmd
@@ -2,7 +2,7 @@
 
 env set bootcmd_hdp ';'
 env set fdtfile ${fdt_prefix}imx8mm-verdin-wifi-dev.dtb
-env set ramdisk_addr_r 0x46400000
+env set ramdisk_addr_r 0x47400000
 env set vidargs 'video=HDMI-A-1:1280x720-16@60D'
 
 env set bootcmd_args 'env set bootargs quiet ${vidargs} initcall_blacklist=vpu_driver_init rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall clk_ignore_unused pci=nomsi ${teziargs}'
