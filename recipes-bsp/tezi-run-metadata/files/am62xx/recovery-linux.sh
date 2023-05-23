0f6486b7d74b8ffac6f1d31a7cecb6957436ed9d
Francesco Dolcini <francesco.dolcini@toradex.com>
2023-05-23 16:52:23 +0200

    tezi-run-metadata: am62: recovery: wait for USB device with lsusb
    Do not rely exclusively on dfu-util -w option to wait for the USB device
    to appear since this is not working when running within an LXC container.
    
    Wait for the device to appear using lsusb every time the device is
    reset.
    
    Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

--- recipes-bsp/tezi-run-metadata/files/am62xx/recovery-linux.sh
+++ recipes-bsp/tezi-run-metadata/files/am62xx/recovery-linux.sh
@@ -11,9 +11,23 @@ fi
 VID_PID_ROM="0451:6165"
 VID_PID_R5="0451:6165"
 VID_PID_A53="0451:6165"
+
+wait_usb_device()
+{
+	VID_PID=$1
+
+	while ! lsusb -d $VID_PID
+	do
+		sleep 1
+	done
+}
+
 # load boot binaries, boot script and tezi fitimage to RAM and boot U-Boot
+wait_usb_device $VID_PID_ROM
 sudo $DFU_UTIL -w -R -a bootloader --device $VID_PID_ROM -D tiboot3-am62x-gp-evm.bin-dfu
+wait_usb_device $VID_PID_R5
 sudo $DFU_UTIL -w -R -a tispl.bin --device $VID_PID_R5 -D tispl.bin
+wait_usb_device $VID_PID_A53
 sudo $DFU_UTIL -w -a u-boot.img --device $VID_PID_A53 -D u-boot.img-recoverytezi
 sudo $DFU_UTIL -w -a ramdisk_addr_r --device $VID_PID_A53 -D tezi.itb
 sudo $DFU_UTIL -w -a loadaddr --device $VID_PID_A53 -D overlays.txt
