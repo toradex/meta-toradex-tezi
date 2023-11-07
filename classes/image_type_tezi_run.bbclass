toradex/meta-toradex-tezi

commit 8ed6953f0e0c597735d3e1b4fdcfaa631ef56d1d

u-boot-toradex: Add mainline u-boot v2022.07 support for imx6ull/imx7
This commit introduces changes to enable the use of Mainline U-Boot
v2022.07 for Colibri-iMX6ULL and Colibri-iMX7, for both NAND and eMMC
devices. The changes include:

- Use of the UBOOT_CONFIG_BASENAME variable, allowing the compilation
  of multiple U-Boot configurations for different machines in the same
  tezi.conf.
- Move U-Boot configuration settings from the machine conf file to the
  distro conf file, making it compatible with other module configurations.
- Change the default name of the u-boot binary for rawnand to append the
  '-rawnand' string, making the binary name different form the eMMC one.
- Update the logic to set the 'supported_product_ids' with the correct
  PIDS.
  As the 'TORADEX_PRODUCT_IDS' moved from the machine configuration to
  the distro configuration, now these IDS are applied to all machines,
  which means
    dtmapping = d.getVarFlags('TORADEX_PRODUCT_IDS')
  will not work anymore. 'd.getVarFlags' returns all flags set, it will
  cause all nand/emmc from iMX7/iMX6ULL to bet set for other machines
  (e.g. Apalis iMX6). The new logic loops trough all IDS and checks
  whether they should be added or not to 'supported_product_ids', based
  on the value set inside 'TORADEX_PRODUCT_IDS[xxxx]'.

Related-to: TEI-1112
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

classes/image_type_tezi_run.bbclass
@@ -5,9 +5,11 @@ EMMCDEVBOOT0:verdin-imx8mp = "emmc-boot0"
 UBOOT_BINARY ??= "u-boot.${UBOOT_SUFFIX}"
 TEZI_CONFIG_FORMAT ?= "4"
 TEZI_AUTO_INSTALL ??= "false"
-TEZI_UBOOT_BINARY_EMMC ??= "${UBOOT_BINARY}"
+TEZI_UBOOT_BINARY_EMMC ??= "${UBOOT_BINARY}-emmc"
 TEZI_UBOOT_BINARY_EMMC:mx8-generic-bsp ??= "${UBOOT_BINARY_TEZI_EMMC}-sd"
-TEZI_UBOOT_BINARY_RAWNAND ??= "${UBOOT_BINARY}"
+TEZI_UBOOT_BINARY_EMMC:apalis-imx6 ??= "${UBOOT_BINARY}-sd"
+TEZI_UBOOT_BINARY_EMMC:colibri-imx6 ??= "${UBOOT_BINARY}-sd"
+TEZI_UBOOT_BINARY_RAWNAND ??= "${UBOOT_BINARY}-rawnand"
 TEZI_UBOOT_BINARY_RECOVERY ??= "${UBOOT_BINARY}-recoverytezi"
 TEZI_UBOOT_BINARY_RECOVERY:mx8-generic-bsp ??= "${UBOOT_BINARY_TEZI_EMMC}-recoverytezi"
 TEZI_UBOOT_BINARY_RECOVERY:am62xx ??= "tiboot3-am62x-gp-verdin.bin-dfu tiboot3-am62x-hs-fs-verdin.bin-dfu u-boot.img-recoverytezi"
@@ -169,17 +171,18 @@ def rootfs_tezi_run_create_json(d, flash_type, type_specific_name = False):
     if product_ids is None:
         bb.fatal("Supported Toradex product ids missing, assign TORADEX_PRODUCT_IDS with a list of product ids.")
 
-    dtmapping = d.getVarFlags('TORADEX_PRODUCT_IDS')
     data["supported_product_ids"] = []
-
-    # If no varflags are set, we assume all product ids supported with single image/U-Boot
-    if dtmapping is not None:
-        for f, v in dtmapping.items():
-            dtbflashtypearr = v.split(',')
-            if len(dtbflashtypearr) < 2 or dtbflashtypearr[1] == flash_type:
-                data["supported_product_ids"].append(f)
-    else:
-        data["supported_product_ids"].extend(product_ids.split())
+    pids_splitted = product_ids.split(' ')
+    for id_num in pids_splitted:
+        dtbflashtype = d.getVarFlag('TORADEX_PRODUCT_IDS', id_num)
+        if dtbflashtype is None:
+            data["supported_product_ids"].append(id_num)
+        else:
+          dtbflashtype_arr = dtbflashtype.split(',')
+          if (len(dtbflashtype_arr) == 1):
+              data["supported_product_ids"].append(id_num)
+          elif (dtbflashtype_arr[1] == flash_type):
+              data["supported_product_ids"].append(id_num)
 
     if flash_type == "rawnand":
         data["mtddevs"] = rootfs_tezi_run_rawnand(d)

conf/distro/tezi.conf
@@ -129,6 +129,48 @@ UBOOT_CONFIG_BASENAME:apalis-imx6 = "apalis_imx6"
 UBOOT_CONFIG:colibri-imx6 = "recoverytezi sd"
 UBOOT_CONFIG_BASENAME:colibri-imx6 = "colibri_imx6"
 
+# The payload offsets are given in number of 512 byte blocks.
+OFFSET_BOOTROM_PAYLOAD:colibri-imx6ull = "2"
+TORADEX_FLASH_TYPE:colibri-imx6ull = "rawnand emmc"
+KERNEL_DEVICETREE:colibri-imx6ull = "\
+    imx6ull-colibri-eval-v3.dtb \
+    imx6ull-colibri-emmc-eval-v3.dtb \
+    imx6ull-colibri-wifi-eval-v3.dtb \
+"
+
+UBOOT_BINARY:colibri-imx6ull = "u-boot.imx"
+UBOOT_CONFIG:colibri-imx6ull = "rawnand emmc recoverytezi"
+UBOOT_CONFIG_BASENAME:colibri-imx6ull = "colibri-imx6ull"
+UBOOT_MACHINE:colibri-imx6ull = ""
+
+# The payload offsets are given in number of 512 byte blocks.
+OFFSET_BOOTROM_PAYLOAD:colibri-imx7 = "2"
+TORADEX_FLASH_TYPE:colibri-imx7 = "rawnand emmc"
+KERNEL_DEVICETREE:colibri-imx7 = "\
+    imx7d-colibri-eval-v3.dtb \
+    imx7d-colibri-emmc-eval-v3.dtb \
+"
+
+UBOOT_BINARY:colibri-imx7 = "u-boot.imx"
+UBOOT_CONFIG:colibri-imx7 = "rawnand emmc recoverytezi"
+UBOOT_CONFIG_BASENAME:colibri-imx7 = "colibri_imx7"
+UBOOT_MACHINE:colibri-imx7 = ""
+
+# As TEZI uses the same build for both eMMC and NAND devices, we need to
+# append the eMMC TORADEX_PRODUCT_IDS as the "colibri-imx7/6ull" will only use
+# the NAND IDS by default
+TORADEX_PRODUCT_IDS:colibri-imx6ull = "0036 0040 0044 0045 0062"
+TORADEX_PRODUCT_IDS:colibri-imx7 = "0032 0033 0039 0041"
+TORADEX_PRODUCT_IDS[0032] = "imx7s-colibri-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0033] = "imx7d-colibri-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0036] = "imx6ull-colibri-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0039] = "imx7d-colibri-emmc-eval-v3.dtb,emmc"
+TORADEX_PRODUCT_IDS[0040] = "imx6ull-colibri-wifi-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0041] = "imx7d-colibri-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0044] = "imx6ull-colibri-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0045] = "imx6ull-colibri-wifi-eval-v3.dtb,rawnand"
+TORADEX_PRODUCT_IDS[0062] = "imx6ull-colibri-emmc-eval-v3.dtb,emmc"
+
 UBOOT_CONFIG:verdin-am62 = "recoverytezi sd"
 UBOOT_CONFIG_BASENAME:verdin-am62 = "verdin-am62_a53"
 # Not needed for the k3r5 case, modify the a53 config to make the generic code
@@ -143,8 +185,8 @@ UBOOT_CONFIG_BASENAME:verdin-imx8mp = "verdin-imx8mp"
 
 UBOOT_CONFIG[recoverytezi] = "${UBOOT_CONFIG_BASENAME}_tezi_defconfig"
 UBOOT_CONFIG[sd] = "${UBOOT_CONFIG_BASENAME}_defconfig"
-
-UBOOT_MACHINE:colibri-imx6ull = ""
+UBOOT_CONFIG[rawnand] = "${UBOOT_CONFIG_BASENAME}_defconfig,,u-boot-nand.imx"
+UBOOT_CONFIG[emmc] = "${@'colibri_imx7_emmc_defconfig' if d.getVar('MACHINE') == 'colibri-imx7' else 'colibri-imx6ull-emmc_defconfig'}"
 
 TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT:apalis-imx6 = "apalis-imx6_vga-640x480_overlay.dtbo apalis-imx6_hdmi_overlay.dtbo"
 TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT:colibri-imx6 = "colibri-imx6_vga-640x480_overlay.dtbo colibri-imx6_hdmi_overlay.dtbo"

recipes-bsp/u-boot/u-boot-toradex-tezi.inc
@@ -30,6 +30,19 @@ do_install:append:colibri-imx7() {
     install_emmc_nand_fw_env
 }
 
+configure_bootcmd_nand () {
+    recoveryconf=${S}/configs/${UBOOT_CONFIG_BASENAME}_defconfig
+    kconfig_configure_variable BOOTCOMMAND \""ubi part ubi ; ubi read \${scriptaddr} bootscr; source \${scriptaddr} ;"\" $recoveryconf
+}
+
+do_configure:prepend:colibri-imx6ull () {
+    configure_bootcmd_nand
+}
+
+do_configure:prepend:colibri-imx7 () {
+    configure_bootcmd_nand
+}
+
 do_configure:prepend:verdin-am62 () {
     kconfig_configure_variable BOOTCOMMAND '"source ${scriptaddr}"' $recoveryconf
 }

commit fd1d4ac8ffb8b8db69cb04e01d887b5bf8cb4dd6

image_type_tezi_run: Fix am62 u-boot regression
A regression was introduced to verdin-am62 machine by commit 8ed6953f,
where the u-boot binary to be flashed to the module is now called
'u-boot.${SUFFIX}-sd' and the verdin has both 'recoverytezi' and 'sd' as
U-Boot configuration.

Fixes: 8ed6953f ("u-boot-toradex: Add mainline u-boot v2022.07 support for imx6ull/imx7")
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

classes/image_type_tezi_run.bbclass
@@ -9,6 +9,7 @@ TEZI_UBOOT_BINARY_EMMC ??= "${UBOOT_BINARY}-emmc"
 TEZI_UBOOT_BINARY_EMMC:mx8-generic-bsp ??= "${UBOOT_BINARY_TEZI_EMMC}-sd"
 TEZI_UBOOT_BINARY_EMMC:apalis-imx6 ??= "${UBOOT_BINARY}-sd"
 TEZI_UBOOT_BINARY_EMMC:colibri-imx6 ??= "${UBOOT_BINARY}-sd"
+TEZI_UBOOT_BINARY_EMMC:am62xx ??= "${UBOOT_BINARY}-sd"
 TEZI_UBOOT_BINARY_RAWNAND ??= "${UBOOT_BINARY}-rawnand"
 TEZI_UBOOT_BINARY_RECOVERY ??= "${UBOOT_BINARY}-recoverytezi"
 TEZI_UBOOT_BINARY_RECOVERY:mx8-generic-bsp ??= "${UBOOT_BINARY_TEZI_EMMC}-recoverytezi"