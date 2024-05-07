677ae8ec217421981fa5c57d3444c010ea236da2
Max Krummenacher <max.krummenacher@toradex.com>
2024-05-07 13:42:26 +0200

    tezi-run-metadata: add aquila-am69
    - Add aquila to 'compatible'.
    - Move files common for K3 SoCs in a k3 folder and add aquila-am69
      specific files.
    - Keep the SocTypeGP.bin file. Dropping the 6byte file would make the
      recipe more convoluted.
    
    Related-to: TEI-1139
    Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
    (cherry picked from commit 8aba7b0d5e52404445964c5adc595ec42ff8a26d)

---
+++ recipes-bsp/tezi-run-metadata/files/aquila-am69/recovery-windows.bat
@@ -0,0 +1,34 @@
+@echo off
+
+echo [93mDownloading Toradex Easy Installer...[0m
+
+set DFU_UTIL=recovery\dfu-util
+set UUU=recovery\uuu.exe
+
+REM USB DFU device vendor and product ID used in the boot ROM, the R5 SPL and the A53 SPL
+set VID_PID_ROM="0451:6167"
+set VID_PID_R5="1b67:4000"
+set VID_PID_A53="1b67:4000"
+
+set TIBOOT3_BIN=tiboot3-am69-hs-fs-aquila.bin
+
+%DFU_UTIL% -w -R -a bootloader --device %VID_PID_ROM% -D %TIBOOT3_BIN%
+%DFU_UTIL% -w -R -a tispl.bin --device %VID_PID_R5% -D tispl.bin
+%DFU_UTIL% -w -R -a u-boot.img --device %VID_PID_A53% -D u-boot.img-recoverytezi
+
+REM call universal update utility (uuu) to download FIT with fastboot
+%UUU% recovery
+
+echo
+IF %ERRORLEVEL% EQU 0 (
+  echo [92mSuccessfully downloaded Toradex Easy Installer.[0m
+) else (
+  echo [91mDownloading Toradex Easy Installer failed...[0m
+)
+pause
+
+goto :EOF
+
+exit /b
+
+:EOF
