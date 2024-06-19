d9d8127ade64d0903e61d536c5c4e4e6c5fa6fa0
Emanuele Ghidoli <emanuele.ghidoli@toradex.com>
2024-06-19 11:51:10 +0200

    tezi: aquila-am69: change A53 references to A72 in script
    Related-to: ELB-5799
    Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

--- recipes-bsp/tezi-run-metadata/files/aquila-am69/recovery-windows.bat
+++ recipes-bsp/tezi-run-metadata/files/aquila-am69/recovery-windows.bat
@@ -3,18 +3,17 @@
 echo [93mDownloading Toradex Easy Installer...[0m
 
 set DFU_UTIL=recovery\dfu-util
-set UUU=recovery\uuu.exe
 
-REM USB DFU device vendor and product ID used in the boot ROM, the R5 SPL and the A53 SPL
+REM USB DFU device vendor and product ID used in the boot ROM, the R5 SPL and the A72 SPL
 set VID_PID_ROM="0451:6167"
 set VID_PID_R5="1b67:4000"
-set VID_PID_A53="1b67:4000"
+set VID_PID_A72="1b67:4000"
 
 set TIBOOT3_BIN=tiboot3-am69-hs-fs-aquila.bin
 
 %DFU_UTIL% -w -R -a bootloader --device %VID_PID_ROM% -D %TIBOOT3_BIN%
 %DFU_UTIL% -w -R -a tispl.bin --device %VID_PID_R5% -D tispl.bin
-%DFU_UTIL% -w -R -a u-boot.img --device %VID_PID_A53% -D u-boot.img-recoverytezi
+%DFU_UTIL% -w -R -a u-boot.img --device %VID_PID_A72% -D u-boot.img-recoverytezi
 
 REM call universal update utility (uuu) to download FIT with fastboot
 %UUU% recovery
