toradex/meta-toradex-tezi

commit e9f062ae811f592911b0c393408922690229ea28

qtbase: optimize size consumption
Remove unnecessary PACKAGECONFIG options applied by the meta-qt5 layer
that are not required for Tezi.

Enable the optimize-size PACKAGECONFIG option to further minimize the
size of all Qt packages. This option makes the compiler optimizes the
binaries for size.

The changes result in the following size reductions:

- qtbase:                      24567 KiB -> 19181 KiB
- qtbase-plugins: 	       2433 KiB -> 1652 KiB
- libqt5waylandclient5:        1104 KiB -> 976 KiB
- libqt5waylandclient-plugins: 663 KiB -> 599 KiB

Related-to: TEI-1232
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

recipes-qt/qt5/qtbase_%.bbappend
@@ -1,0 +1,2 @@
+PACKAGECONFIG:remove = "accessibility udev evdev tools"
+PACKAGECONFIG:append = " optimize-size"

commit 8068055fc099467b914dcf3d5ec3d2a5efc8e7df

qtbase: fix build error with TI modules
The meta-toradex-ti layer qtbase bbappend adds libinput as a
PACKAGECONFIG option, which fails to build without the udev option.

| ERROR: Feature 'libinput' was enabled, but the pre-condition
'features.libudev && libs.libinput' failed.

According to tests and Qt documentation [1], libinput is only required
if no windowing system is present to handle input devices. Since Tezi
uses Weston, libinput is unnecessary and its removal does not cause
issues to Tezi GUI.

To resolve the build error, remove the libinput option from
PACKAGECONFIG.

[1] https://doc.qt.io/qt-5/inputs-linux-device.html

Related-to: TEI-1232
Fixes: e9f062ae811f ("qtbase: optimize size consumption")
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

recipes-qt/qt5/qtbase_%.bbappend
@@ -1,2 +1,2 @@
-PACKAGECONFIG:remove = "accessibility udev evdev tools"
+PACKAGECONFIG:remove = "accessibility udev evdev tools libinput"
 PACKAGECONFIG:append = " optimize-size"