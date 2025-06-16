toradex/meta-toradex-tezi

commit e0453742933ca7b8e602e3b53fae05c7a1eea00e

tezi.itb: follow oe fitimage changes
OE-Core changed the way fitImages are created [1].

The fitImage creation is now in its own recipe using deployed
artifacts from the virtual/kernel recipe. Thus make sure that
virtual/kernel is deploying those artifacts by using the
new kernel-fit-extra-artifacts class and depend on the fitImage
creation recipe when putting together the tezi image.

Also the configuration now no longer contain any path prefix
to the device tree files. ('nxp_imx_' | 'ti_') so remove them
from our boot script so that the configuration is found at runtime.

[1] https://lore.kernel.org/all/20250603082419.409564-1-adrian.freihofer@siemens.com/

Related-to: TEI-1264
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

classes/image_type_tezi_run.bbclass
@@ -268,5 +268,5 @@ IMAGE_CMD:tezirunimg () {
 do_image_tezirunimg[dirs] += "${WORKDIR}/${TDX_VER_ID} ${WORKDIR}"
 do_image_tezirunimg[cleandirs] += "${WORKDIR}/${TDX_VER_ID}"
 do_image_tezirunimg[prefuncs] += "rootfs_tezirun_run_json"
-do_image_tezirunimg[depends] += "virtual/bootloader:do_deploy u-boot-distro-boot:do_deploy virtual/kernel:do_deploy tezi-run-metadata:do_deploy uuu-bin:do_deploy"
+do_image_tezirunimg[depends] += "virtual/bootloader:do_deploy u-boot-distro-boot:do_deploy linux-yocto-fitimage:do_deploy tezi-run-metadata:do_deploy uuu-bin:do_deploy"
 do_image_teziimg[vardepsexclude] = "DISTRO_VERSION TDX_VER_ID DATE"

conf/distro/tezi.conf
@@ -97,12 +97,7 @@ COPY_LIC_MANIFEST = "1"
 ERROR_QA:remove = "version-going-backwards"
 
 # Explicitly use fitimage
-KERNEL_CLASSES:append = " kernel-fitimage"
-KERNEL_IMAGETYPE:forcevariable = "fitImage"
-KERNEL_ALT_IMAGETYPE = "zImage"
-KERNEL_ALT_IMAGETYPE:mx8-generic-bsp = "Image"
-KERNEL_ALT_IMAGETYPE:mx9-generic-bsp = "Image"
-KERNEL_ALT_IMAGETYPE:k3 = "Image"
+KERNEL_CLASSES:append = " kernel-fit-extra-artifacts"
 
 # Make sure kernel does not get deployed into rootfs
 RRECOMMENDS:${KERNEL_PACKAGE_NAME}-base = ""

recipes-core/u-boot-distro-boot/files/apalis-imx6/boot.cmd.in
@@ -19,7 +19,7 @@ env set fdt_high
 env set fdt_resize true
 
 env set bootcmd_args 'env set bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480D video=HDMI-A-1:640x480-16@60D video=LVDS-1:d video=VGA-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'

recipes-core/u-boot-distro-boot/files/apalis-imx8/boot.cmd.in
@@ -11,7 +11,7 @@ env set fitimage_addr 0x8a000000
 env set overlays_addr 0x82e10000
 
 env set bootcmd_args 'env set bootargs quiet video=LVDS-1:d video=HDMI-A-1:640x480-16@60D initcall_blacklist=vpu_driver_init rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'

recipes-core/u-boot-distro-boot/files/aquila-am69/boot.cmd.in
@@ -10,7 +10,7 @@ test -n ${overlays_file} || env set overlays_file "overlays.txt"
 env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
 
 env set bootcmd_args 'env set bootargs quiet video=DP-1:640x480@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${ramdisk_addr_r}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${ramdisk_addr_r}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'

recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd.in
@@ -19,7 +19,7 @@ env set fdt_high
 env set fdt_resize true
 
 env set bootcmd_args 'env set bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480-16@60D video=HDMI-A-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'

recipes-core/u-boot-distro-boot/files/colibri-imx6ull/boot.cmd.in
@@ -10,7 +10,7 @@ test -n ${overlays_file} || env set overlays_file "overlays.txt"
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
 env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
 
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@imx6ull-colibri\${variant}-\${fdt_board}.dtb\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-imx6ull-colibri\${variant}-\${fdt_board}.dtb\${fitconf_fdt_overlays}'
 
 test -n ${devtype} || env set devtype mmc
 test -n ${devnum} || env set devnum 0

recipes-core/u-boot-distro-boot/files/colibri-imx7/boot.cmd.in
@@ -10,7 +10,7 @@ test -n ${overlays_file} || env set overlays_file "overlays.txt"
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
 env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
 
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${soc}-colibri\${variant}-\${fdt_board}.dtb\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${soc}-colibri\${variant}-\${fdt_board}.dtb\${fitconf_fdt_overlays}'
 
 test -n ${devtype} || env set devtype mmc
 test -n ${devnum} || env set devnum 0

commit 0d8262545c44bc17dc8a5105ff02dbc14f48a135

tezi.itb: rename while installing
Rather than creating a symlink 'tezi.itb' use the fitImage file and
rename to 'tezi.itb' while copying during image creation.

Remove the symlink creation as part of the linux kernel build which
now did become a no operation.

Related-to: TEI-1264
Fixes: e0453742933c ("tezi.itb: follow oe fitimage changes")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

classes/image_type_tezi_run.bbclass
@@ -33,7 +33,7 @@ DEPENDS += "u-boot-mkimage-native zip-native"
 
 def fitimg_get_size(d):
     import os
-    return os.path.getsize(os.path.join(d.getVar('DEPLOY_DIR_IMAGE'), 'tezi.itb')) / (1024 * 1024)
+    return os.path.getsize(os.path.join(d.getVar('DEPLOY_DIR_IMAGE'), 'fitImage')) / (1024 * 1024)
 
 def rootfs_tezi_run_emmc(d):
     from collections import OrderedDict
@@ -259,7 +259,8 @@ IMAGE_CMD:tezirunimg () {
         done
     fi
 
-    (cd ${DEPLOY_DIR_IMAGE}; cp -L -R ${TEZI_IMAGE_UBOOT_FILES} ${TEZI_UBOOT_BINARY_RECOVERY} ${MACHINE_BOOT_FILES} tezi.itb tezi-run-metadata/* uuu-bin/* ${WORKDIR}/${TDX_VER_ID})
+    (cd ${DEPLOY_DIR_IMAGE}; cp -L -R ${TEZI_IMAGE_UBOOT_FILES} ${TEZI_UBOOT_BINARY_RECOVERY} ${MACHINE_BOOT_FILES} tezi-run-metadata/* uuu-bin/* ${WORKDIR}/${TDX_VER_ID})
+    (cd ${DEPLOY_DIR_IMAGE}; cp -L fitImage ${WORKDIR}/${TDX_VER_ID}/tezi.itb)
     (cd ${IMGDEPLOYDIR}; cp -L -R ${TEZI_IMAGE_FILES} $LEGACY_IMAGE_FILES ${WORKDIR}/${TDX_VER_ID})
     zip -r ${TDX_VER_ID}.zip ${TDX_VER_ID}
     mv ${TDX_VER_ID}.zip ${IMGDEPLOYDIR}

recipes-kernel/linux/linux-toradex-tezi-common.inc
@@ -1,8 +1,0 @@
-kernel_do_deploy:append () {
-	# Create tezi.itb symbolic link
-	if echo ${KERNEL_IMAGETYPES} | grep -wq "fitImage"; then
-		if [ -n "${INITRAMFS_IMAGE}" ]; then
-			ln -snf fitImage-${INITRAMFS_IMAGE_NAME}-${KERNEL_FIT_NAME}.bin ${DEPLOYDIR}/tezi.itb
-		fi
-	fi
-}

recipes-kernel/linux/linux-toradex-ti_%.bbappend
@@ -1,5 +1,3 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 
-require linux-toradex-tezi-common.inc
-
 SRC_URI:append = " file://tezi.cfg"

recipes-kernel/linux/linux-toradex-upstream_%.bbappend
@@ -1,7 +1,5 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:${THISDIR}/files:"
 
-require linux-toradex-tezi-common.inc
-
 SRC_URI:append = " \
     file://0001-MLK-11747-mtd-nand-raw-gpmi-nand-add-the-debugfs-fil.patch \
     file://0002-ARM-dts-imx7-colibri-use-static-MTD-partition-layout.patch \

recipes-kernel/linux/linux-toradex_%.bbappend
@@ -1,7 +1,6 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}-imx:${THISDIR}/files:"
 
 inherit toradex-kernel-config
-require linux-toradex-tezi-common.inc
 
 SRC_URI:append = " \
     file://0001-drm-bridge-cadence-lower-mhdp-hdmi-bridge-ddc-edid-r.patch \