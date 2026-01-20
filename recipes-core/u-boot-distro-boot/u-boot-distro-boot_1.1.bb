toradex/meta-toradex-tezi

commit 6a15879367cf168630e8d85b77a813a4ecb59b57

u-boot-distro-boot: unify tezi boot scripts
Replaces multiple device-specific boot.cmd.in files with a unified
boot.cmd.in template that uses configurable variables. Platform-specific
differences are now handled through machine-specific replacements in the
recipe.

Key features of this change:
- Created two base boot scripts: boot.cmd.in and boot.cmd.in-NAND (for
  NAND devices)
- Added machine-specific variables to add specific variables
- Unified NAND boot script to handle both imx6ull and imx7 devices
- fdt_high and fdt_resize were removed because tests on Scarthgap show
  these variables are not needed anymore.
- console inside boot args removed in favor of a generic script to
  create the console argument dinamically
- initcall_blacklist=vpu_driver_init removed since tests shows this is
  not required anymore.

This reduces code duplication and make it easier for future maintenance
and new hardware additions.

Related-to: TEI-1301
Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -5,14 +5,66 @@ LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda
 DEPENDS = "u-boot-mkimage-native"
 INHIBIT_DEFAULT_DEPS = "1"
 
-SRC_URI = "file://boot.cmd.in"
+SRC_URI = "file://boot.cmd.in file://boot.cmd.in-NAND"
 
 DTB_PREFIX ??= "${@d.getVar('KERNEL_DTB_PREFIX').replace("/", "_") if d.getVar('KERNEL_DTB_PREFIX') else ''}"
 
 inherit deploy nopackages
 
+TEZI_BOOT_ARGS = ""
+TEZI_BOOT_ARGS:apalis-imx6 = "video=DPI-1:640x480D video=HDMI-A-1:640x480-16@60D video=LVDS-1:d video=VGA-1:640x480-16@60D"
+TEZI_BOOT_ARGS:apalis-imx8 = "video=LVDS-1:d video=HDMI-A-1:640x480-16@60D"
+TEZI_BOOT_ARGS:aquila-am69 = "video=DP-1:1280x720@60D"
+TEZI_BOOT_ARGS:aquila-imx95 = "video=DP-1:1280x720@60D"
+TEZI_BOOT_ARGS:colibri-imx6 = "video=DPI-1:640x480-16@60D video=HDMI-A-1:640x480-16@60D"
+TEZI_BOOT_ARGS:colibri-imx6ull = "video=DPI-1:640x480@60D"
+TEZI_BOOT_ARGS:colibri-imx7 = "video=DPI-1:640x480@60D"
+TEZI_BOOT_ARGS:colibri-imx8x = "video=DPI-1:640x480-16@60D"
+TEZI_BOOT_ARGS:toradex-smarc-imx8mp = "video=HDMI-A-1:1280x720-16@60D"
+TEZI_BOOT_ARGS:toradex-smarc-imx95 = "video=DP-1:1280x720@60D"
+TEZI_BOOT_ARGS:verdin-am62 = "video=HDMI-A-1:640x480-16@60D"
+TEZI_BOOT_ARGS:verdin-am62p = "video=HDMI-A-1:1280x720-16@60D"
+TEZI_BOOT_ARGS:verdin-imx8mm = "video=HDMI-A-1:1280x720-16@60D"
+TEZI_BOOT_ARGS:verdin-imx8mp = "video=HDMI-A-1:1280x720-16@60D video=HDMI-A-2:1280x720-16@60D"
+TEZI_BOOT_ARGS:verdin-imx95 = "video=HDMI-A-1:1280x720-16@60D"
+
+# As default, keep the ramdisk_addr_r and loadaddr from u-boot for the fitimage
+# and overlay files. This way TI machines can use this as default value.
+TEZI_FITIMAGE_ADDR = "${ramdisk_addr_r}"
+TEZI_OVERLAY_ADDR = "${loadaddr}"
+# These are handcrafted values that make sure the tezi.itb file can be loaded
+# to RAM without corrputing or overlaping any other files in memory.  Values
+# not described here will use the ramdisk_add_r instead, which (in these cases)
+# are working fine.
+TEZI_FITIMAGE_ADDR:apalis-imx6 = "0x14420000"
+TEZI_FITIMAGE_ADDR:apalis-imx8 = "0x8a000000"
+TEZI_FITIMAGE_ADDR:colibri-imx6 = "0x14420000"
+TEZI_FITIMAGE_ADDR:colibri-imx6ull = "0x81100000"
+TEZI_FITIMAGE_ADDR:colibri-imx7 = "0x81100000"
+TEZI_FITIMAGE_ADDR:colibri-imx8x = "0x8a000000"
+TEZI_FITIMAGE_ADDR:mx8mp-generic-bsp = "0x44200000"
+TEZI_FITIMAGE_ADDR:mx95-generic-bsp = "0x90400000"
+TEZI_FITIMAGE_ADDR:verdin-imx8mm = "0x44200000"
+
+TEZI_OVERLAY_ADDR:apalis-imx6 = "0x14410000"
+TEZI_OVERLAY_ADDR:apalis-imx8 = "0x82e10000"
+TEZI_OVERLAY_ADDR:colibri-imx6 = "0x14410000"
+TEZI_OVERLAY_ADDR:colibri-imx6ull = "0x80f00000"
+TEZI_OVERLAY_ADDR:colibri-imx7 = "0x80f00000"
+TEZI_OVERLAY_ADDR:colibri-imx8x = "0x82e10000"
+TEZI_OVERLAY_ADDR:mx8mp-generic-bsp = "0x42e10000"
+TEZI_OVERLAY_ADDR:mx95-generic-bsp = "0x9c800000"
+TEZI_OVERLAY_ADDR:verdin-imx8mm = "0x42e10000"
+
+BOOT_CMD_FILE = "boot.cmd.in"
+BOOT_CMD_FILE:colibri-imx6ull = "boot.cmd.in-NAND"
+BOOT_CMD_FILE:colibri-imx7 = "boot.cmd.in-NAND"
+
 do_deploy () {
-    sed 's/@@INITRAMFS_FSTYPES@@/${INITRAMFS_FSTYPES}/' ${WORKDIR}/boot.cmd.in > boot.cmd
+    sed 's/@@INITRAMFS_FSTYPES@@/${INITRAMFS_FSTYPES}/' ${WORKDIR}/${BOOT_CMD_FILE} > boot.cmd
+    sed -i 's/@@TEZI_BOOT_ARGS@@/${TEZI_BOOT_ARGS}/' boot.cmd
+    sed -i 's/@@TEZI_FITIMAGE_ADDR@@/${TEZI_FITIMAGE_ADDR}/' boot.cmd
+    sed -i 's/@@TEZI_OVERLAY_ADDR@@/${TEZI_OVERLAY_ADDR}/' boot.cmd
     sed -i 's/@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@/${TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT}/' boot.cmd
     sed -i 's/@@KERNEL_DTB_PREFIX@@/${DTB_PREFIX}/' boot.cmd
 

recipes-core/u-boot-distro-boot/files/apalis-imx6/boot.cmd.in
@@ -1,45 +1,0 @@
-# apalis-imx6 TEZI script
-
-test -n ${devtype} || env set devtype mmc
-test -n ${devnum} || env set devnum 0
-test -n ${distro_bootpart} || env set distro_bootpart 1
-
-env set fitconf_fdt_overlays
-env set prefix
-test -n ${overlays_file} || env set overlays_file "overlays.txt"
-env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
-# This address needs to be the alligned with the one used by imx_usb/uuu loader
-# inside the 'recovery' folder
-env set fitimage_addr 0x14420000
-env set overlays_addr 0x14410000
-
-# Re-enable fdt relocation since in place fdt edits corrupt the ramdisk
-# in a FIT image...
-env set fdt_high
-env set fdt_resize true
-
-env set bootcmd_args 'env set bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480D video=HDMI-A-1:640x480-16@60D video=LVDS-1:d video=VGA-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
-env set tezi_image ${prefix}tezi.itb
-
-env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
-
-if test x${recovery_tezi} = x1
-then
-    # ${tezi_image} has been loaded over usb, don't try to load from eMMC
-    env set bootcmd_tezi ';'
-    env set load_overlays_file 'env import -t ${overlays_addr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'
-else
-    env set bootcmd_tezi '${load_cmd} ${fitimage_addr} ${tezi_image}'
-    env set load_overlays_file '${load_cmd} ${overlays_addr} ${overlays_file} && env import -t ${overlays_addr} ${filesize}; test -n ${fdt_overlays} || run set_default_overlays'
-fi
-
-env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
-
-run load_overlays_file
-run set_apply_overlays
-run apply_overlays
-
-run bootcmd_args
-run bootcmd_tezi
-run bootcmd_run

recipes-core/u-boot-distro-boot/files/apalis-imx8/boot.cmd.in
@@ -1,37 +1,0 @@
-# apalis-imx8 hardware setup
-
-env set fitconf_fdt_overlays
-env set prefix
-test -n ${overlays_file} || env set overlays_file "overlays.txt"
-env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
-
-# This address needs to be the alligned with the one used by imx_usb/uuu loader
-# inside the 'recovery' folder
-env set fitimage_addr 0x8a000000
-env set overlays_addr 0x82e10000
-
-env set bootcmd_args 'env set bootargs quiet video=LVDS-1:d video=HDMI-A-1:640x480-16@60D initcall_blacklist=vpu_driver_init rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
-env set tezi_image ${prefix}tezi.itb
-
-env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
-
-if test x${recovery_tezi} = x1
-then
-    # ${tezi_image} has been loaded over usb, don't try to load from eMMC
-    env set bootcmd_tezi ';'
-    env set load_overlays_file 'env import -t ${overlays_addr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'
-else
-    env set bootcmd_tezi '${load_cmd} ${fitimage_addr} ${tezi_image}'
-    env set load_overlays_file '${load_cmd} ${overlays_addr} ${overlays_file} && env import -t ${overlays_addr} ${filesize}; test -n ${fdt_overlays} || run set_default_overlays'
-fi
-
-env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
-
-run load_overlays_file
-run set_apply_overlays
-run apply_overlays
-
-run bootcmd_args
-run bootcmd_tezi
-run bootcmd_run

recipes-core/u-boot-distro-boot/files/aquila-am69/boot.cmd.in
@@ -1,36 +1,0 @@
-# aquila-am69 TEZI bootscript
-
-test -n ${devtype} || env set devtype mmc
-test -n ${devnum} || env set devnum 0
-test -n ${distro_bootpart} || env set distro_bootpart 1
-
-env set fitconf_fdt_overlays
-env set prefix
-test -n ${overlays_file} || env set overlays_file "overlays.txt"
-env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
-
-env set bootcmd_args 'env set bootargs quiet video=DP-1:1280x720@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${ramdisk_addr_r}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
-env set tezi_image ${prefix}tezi.itb
-
-env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
-
-if test x${recovery_tezi} = x1
-then
-    # ${tezi_image} has been loaded over dfu, don't try to load from eMMC
-    env set bootcmd_tezi ';'
-    env set load_overlays_file 'env import -t ${loadaddr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'
-else
-    env set bootcmd_tezi '${load_cmd} ${ramdisk_addr_r} ${tezi_image}'
-    env set load_overlays_file '${load_cmd} ${loadaddr} ${overlays_file} && env import -t ${loadaddr} ${filesize}; test -n ${fdt_overlays} || run set_default_overlays'
-fi
-
-env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
-
-run load_overlays_file
-run set_apply_overlays
-run apply_overlays
-
-run bootcmd_args
-run bootcmd_tezi
-run bootcmd_run

recipes-core/u-boot-distro-boot/files/aquila-imx95/boot.cmd.in
@@ -1,37 +1,0 @@
-# aquila-imx95 TEZI boot script
-
-env set fitconf_fdt_overlays
-env set prefix
-test -n ${overlays_file} || env set overlays_file "overlays.txt"
-env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
-
-# This address needs to be the alligned with the one used by imx_usb/uuu loader
-# inside the 'recovery' folder
-env set fitimage_addr 0x90400000
-env set overlays_addr 0x9c800000
-
-env set bootcmd_args 'env set bootargs quiet video=DP-1:1280x720-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
-env set tezi_image ${prefix}tezi.itb
-
-env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
-
-if test x${recovery_tezi} = x1
-then
-    # ${tezi_image} has been loaded over usb, don't try to load from eMMC
-    env set bootcmd_tezi ';'
-    env set load_overlays_file 'env import -t ${overlays_addr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'
-else
-    env set bootcmd_tezi '${load_cmd} ${fitimage_addr} ${tezi_image}'
-    env set load_overlays_file '${load_cmd} ${overlays_addr} ${overlays_file} && env import -t ${overlays_addr} ${filesize}; test -n ${fdt_overlays} || run set_default_overlays'
-fi
-
-env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
-
-run load_overlays_file
-run set_apply_overlays
-run apply_overlays
-
-run bootcmd_args
-run bootcmd_tezi
-run bootcmd_run

recipes-core/u-boot-distro-boot/files/boot.cmd.in
@@ -1,5 +1,8 @@
-# TODO: adapt to toradex-smarc-imx95 specifics
-# toradex-smarc-imx95 TEZI boot script
+# TEZI script
+
+test -n ${devtype} || env set devtype mmc
+test -n ${devnum} || env set devnum 0
+test -n ${distro_bootpart} || env set distro_bootpart 1
 
 env set fitconf_fdt_overlays
 env set prefix
@@ -8,10 +11,16 @@ env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
 
 # This address needs to be the alligned with the one used by imx_usb/uuu loader
 # inside the 'recovery' folder
-env set fitimage_addr 0x90400000
-env set overlays_addr 0x9c800000
+env set fitimage_addr @@TEZI_FITIMAGE_ADDR@@
+env set overlays_addr @@TEZI_OVERLAY_ADDR@@
 
-env set bootcmd_args 'env set bootargs quiet video=DP-1:1280x720-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
+# Set console baudrate only when required
+setexpr _res sub "," "_" ${console}
+if test "${_res}" = "" -o "${_res}" = "${console}"; then
+	env set console "${console},${baudrate}"
+fi
+
+env set bootcmd_args 'env set bootargs @@TEZI_BOOT_ARGS@@ console=${console} quiet rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
 env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 

recipes-core/u-boot-distro-boot/files/boot.cmd.in-NAND
@@ -1,16 +1,23 @@
-# colibri-imx7 TEZI script
+# TEZI script for devices with NAND options
 
-env set bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}
+# Set console baudrate only when required
+setexpr _res sub "," "_" ${console}
+if test "${_res}" = "" -o "${_res}" = "${console}"; then
+	env set console "${console},${baudrate}"
+fi
+
+env set bootargs @@TEZI_BOOT_ARGS@@ console=${console} quiet rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}
+
 # Addresses defined by uuu.auto script
-env set fitimage_addr 0x81100000
-env set overlays_addr 0x80f00000
+env set fitimage_addr @@TEZI_FITIMAGE_ADDR@@
+env set overlays_addr @@TEZI_OVERLAY_ADDR@@
 
 env set fitconf_fdt_overlays
 test -n ${overlays_file} || env set overlays_file "overlays.txt"
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
 env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
 
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${soc}-colibri\${variant}-\${fdt_board}.dtb\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
 
 test -n ${devtype} || env set devtype mmc
 test -n ${devnum} || env set devnum 0
@@ -18,14 +25,14 @@ test -n ${distro_bootpart} || env set distro_bootpart 1
 
 if test x${recovery_tezi} != x1
 then
-    # If variant is not set, then we're in a NAND device. Otherwise, if variant
-    # is "-emmc", this a an eMMC device.
-    if test x${variant} = x
+    # If variant is '-emmc', this is an eMMC device
+    if test x${variant} = x-emmc
     then
-        ubi read ${fitimage_addr} rootfs
-    else
         env set load_overlays_file 'load ${devtype} ${devnum}:${distro_bootpart} ${overlays_addr} ${overlays_file} && env import -t ${overlays_addr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'
         load ${devtype} ${devnum}:${distro_bootpart} ${fitimage_addr} ${prefix}tezi.itb
+    # If variant is not set or it's 'wifi', then we're in a NAND device.
+    else
+        ubi read ${fitimage_addr} rootfs
     fi
 else
     env set load_overlays_file 'env import -t ${overlays_addr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'

recipes-core/u-boot-distro-boot/files/colibri-imx6/boot.cmd.in
@@ -1,45 +1,0 @@
-# colibri-imx6 TEZI script
-
-test -n ${devtype} || env set devtype mmc
-test -n ${devnum} || env set devnum 0
-test -n ${distro_bootpart} || env set distro_bootpart 1
-
-env set fitconf_fdt_overlays
-env set prefix
-test -n ${overlays_file} || env set overlays_file "overlays.txt"
-env set load_cmd load ${devtype} ${devnum}:${distro_bootpart}
-# This address needs to be the alligned with the one used by imx_usb/uuu loader
-# inside the 'recovery' folder
-env set fitimage_addr 0x14420000
-env set overlays_addr 0x14410000
-
-# Re-enable fdt relocation since in place fdt edits corrupt the ramdisk
-# in a FIT image...
-env set fdt_high
-env set fdt_resize true
-
-env set bootcmd_args 'env set bootargs console=ttymxc0,115200 quiet video=DPI-1:640x480-16@60D video=HDMI-A-1:640x480-16@60D rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
-env set tezi_image ${prefix}tezi.itb
-
-env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
-
-if test x${recovery_tezi} = x1
-then
-    # ${tezi_image} has been loaded over usb, don't try to load from eMMC
-    env set bootcmd_tezi ';'
-    env set load_overlays_file 'env import -t ${overlays_addr} 0x200; test -n ${fdt_overlays} || run set_default_overlays'
-else
-    env set bootcmd_tezi '${load_cmd} ${fitimage_addr} ${tezi_image}'
-    env set load_overlays_file '${load_cmd} ${overlays_addr} ${overlays_file} && env import -t ${overlays_addr} ${filesize}; test -n ${fdt_overlays} || run set_default_overlays'
-fi
-
-env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
-
-run load_overlays_file
-run set_apply_overlays
-run apply_overlays
-
-run bootcmd_args
-run bootcmd_tezi
-run bootcmd_run

commit f8e3449d0277d25ab01b2aefc1ff64e3a1ffd3a2

u-boot-distro-boot: fix distorted HDMI output on verdin-imx8mm
The HDMI output on Verdin iMX8MM was distorted because of an already
known color issue at 16 bpp color depth, as reported in 7d420ac174e4
("u-boot-distro-boot: verdin-imx8mm: boot.cmd.in: update kernel arguments").

While unifying tezi boot scripts the specific configuration was not
correctly reported in u-boot-distro-boot_1.1.bb.

Fix the issue by restoring the correct kernel argument.

Related-to: TEI-1312
Fixes: 6a15879367cf ("u-boot-distro-boot: unify tezi boot scripts")
Signed-off-by: Emanuele Ghidoli <emanuele.ghidoli@toradex.com>

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -26,7 +26,7 @@ TEZI_BOOT_ARGS:toradex-smarc-imx8mp = "video=HDMI-A-1:1280x720-16@60D"
 TEZI_BOOT_ARGS:toradex-smarc-imx95 = "video=DP-1:1280x720@60D"
 TEZI_BOOT_ARGS:verdin-am62 = "video=HDMI-A-1:640x480-16@60D"
 TEZI_BOOT_ARGS:verdin-am62p = "video=HDMI-A-1:1280x720-16@60D"
-TEZI_BOOT_ARGS:verdin-imx8mm = "video=HDMI-A-1:1280x720-16@60D"
+TEZI_BOOT_ARGS:verdin-imx8mm = "video=HDMI-A-1:1280x720@60D"
 TEZI_BOOT_ARGS:verdin-imx8mp = "video=HDMI-A-1:1280x720-16@60D video=HDMI-A-2:1280x720-16@60D"
 TEZI_BOOT_ARGS:verdin-imx95 = "video=HDMI-A-1:1280x720-16@60D"
 