toradex/meta-toradex-tezi

commit 8afcde346e3e3fda3a7aed5fc2e51310f2666059

versioning: change to use latest toradex versioning scheme
Rework the versioning to be a copy of how it is done in the BSP.
The distro variables map as follow:
old			new
TDX_VER_ITEM		TDX_RELEASE
TDX_VER_PACKAGE_MIN	TDX_VERSION

In image.json the "release_date" formating changed to:
"20181019"		"2021-04-20"

Additionally the include file providing the versioning has been moved
from conf/tdx_version.conf to conf/distro/tdx_version.inc were it belongs.

Related-to: TEI-143
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

classes/image_type_tezi_run.bbclass
@@ -106,6 +106,7 @@ def rootfs_tezi_run_rawnand(d):
 def rootfs_tezi_run_create_json(d, flash_type, type_specific_name = False):
     import json
     from collections import OrderedDict
+    from datetime import datetime
 
     deploydir = d.getVar('DEPLOY_DIR_IMAGE')
 
@@ -114,8 +115,8 @@ def rootfs_tezi_run_create_json(d, flash_type, type_specific_name = False):
     # Use image recipes SUMMARY/DESCRIPTION/PV...
     data["name"] = d.getVar('SUMMARY')
     data["description"] = d.getVar('DESCRIPTION')
-    data["version"] = d.getVar('TDX_VER_PACKAGE_MIN')
-    data["release_date"] = d.getVar('TDX_VERDATE')[1:9]
+    data["version"] = d.getVar('TDX_VERSION')
+    data["release_date"] = datetime.strptime(d.getVar('DATE', False), '%Y%m%d').date().isoformat()
     if os.path.exists(os.path.join(deploydir, "tezi-run-metadata", "wrapup.sh")):
         data["wrapup_script"] = "wrapup.sh"
     if os.path.exists(os.path.join(deploydir, "tezi-run-metadata", "tezi.png")):
@@ -183,3 +184,4 @@ do_image_tezirunimg[prefuncs] += "rootfs_tezirun_run_json"
 do_image_tezirunimg[depends] += "virtual/bootloader:do_deploy u-boot-distro-boot:do_deploy virtual/kernel:do_deploy tezi-run-metadata:do_deploy \
                                  ${@'%s:do_deploy' % d.getVar('IMAGE_BOOTLOADER') if d.getVar('IMAGE_BOOTLOADER') else ''} \
                                 "
+do_image_teziimg[vardepsexclude] = "DATE"

conf/distro/tdx_version.inc
@@ -1,0 +1,43 @@
+# These variables are used to set image, kernel and U-Boot versions
+
+TDX_BUILDNBR ?= "0"
+TDX_PURPOSE ?= "Testing"
+
+def get_tdx_prerelease(purpose, datetime):
+    if purpose == "Testing":
+        return '-devel-%s' % (datetime)
+    elif purpose == "Nightly":
+        return '-devel-%s' % (datetime[:8])
+    elif purpose == "Monthly":
+        return '-devel-%s' % (datetime[:6])
+    elif purpose == "Release":
+        return ''
+    else:
+        return '-devel-%s' % (datetime)
+
+TDX_BUILD ?= "+build.${TDX_BUILDNBR}"
+TDX_MATRIX_BUILD_TIME ?= "${DATETIME}"
+TDX_MATRIX_BUILD_TIME[vardepsexclude] = "DATETIME"
+TDX_PRERELEASE ?= "${@get_tdx_prerelease(d.getVar('TDX_PURPOSE'), d.getVar('TDX_MATRIX_BUILD_TIME'))}"
+TDX_MAJOR ?= "5"
+TDX_MINOR ?= "3"
+TDX_PATCH ?= "0"
+TDX_RELEASE ?= "${TDX_MAJOR}.${TDX_MINOR}.${TDX_PATCH}"
+
+# Toradex Version number without date for U-Boot/Kernel and other Toradex
+# specific software artifacts.
+TDX_VERSION ?= "${TDX_RELEASE}${@'' if d.getVar('TDX_PURPOSE') == 'Release' else '-devel'}"
+
+# Complete image version with date and build number
+DISTRO_VERSION = "${TDX_RELEASE}${TDX_PRERELEASE}${TDX_BUILD}"
+
+TDX_VER_ID_apalis-imx6 = "Apalis-iMX6_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_apalis-imx8 = "Apalis-iMX8_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_apalis-imx8x = "Apalis-iMX8X_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_apalis-tk1 = "Apalis-TK1_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_colibri-imx6 = "Colibri-iMX6_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_colibri-imx6ull = "Colibri-iMX6ULL_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_colibri-imx7 = "Colibri-iMX7_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_colibri-imx7-emmc = "Colibri-iMX7-eMMC_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_colibri-imx8x = "Colibri-iMX8X_ToradexEasyInstaller_${DISTRO_VERSION}"
+TDX_VER_ID_verdin-imx8mm = "Verdin-iMX8MM_ToradexEasyInstaller_${DISTRO_VERSION}"

conf/distro/tezi.conf
@@ -141,4 +141,4 @@ UBOOT_MACHINE_FW_UTILS_colibri-imx6ull="colibri-imx6ull_defconfig"
 UBOOT_MACHINE_FW_UTILS_colibri-imx7="colibri_imx7_defconfig"
 UBOOT_MACHINE_FW_UTILS_colibri-imx7-emmc="colibri_imx7_emmc_defconfig"
 
-require conf/tdx_version.conf
+require conf/distro/tdx_version.inc

conf/tdx_version.conf
@@ -1,24 +1,0 @@
-# These variables are used to set image, kernel and U-Boot versions
-
-TDX_VERMAJOR = "2"
-TDX_VERMINOR = "0"
-TDX_VERBETA = "b8"
-TDX_VERPATCH = "8"
-TDX_VERNIGHTLY ?= ""
-TDX_VERDATE ?= "-${DATE}"
-TDX_VERDATE[vardepsexclude] = "DATE"
-
-TDX_VER_PACKAGE_MIN = "${TDX_VERMAJOR}.${TDX_VERMINOR}${TDX_VERBETA}${TDX_VERNIGHTLY}"
-TDX_VER_PACKAGE = "${TDX_VER_PACKAGE_MIN}${TDX_VERDATE}"
-TDX_VER_ITEM = "${TDX_VERMAJOR}.${TDX_VERMINOR}.${TDX_VERPATCH}"
-
-TDX_VER_ID_apalis-imx6 = "Apalis-iMX6_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_apalis-imx8 = "Apalis-iMX8_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_apalis-imx8x = "Apalis-iMX8X_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_apalis-tk1 = "Apalis-TK1_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_colibri-imx6 = "Colibri-iMX6_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_colibri-imx6ull = "Colibri-iMX6ULL_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_colibri-imx7 = "Colibri-iMX7_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_colibri-imx7-emmc = "Colibri-iMX7-eMMC_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_colibri-imx8x = "Colibri-iMX8X_ToradexEasyInstaller_${TDX_VER_PACKAGE}"
-TDX_VER_ID_verdin-imx8mm = "Verdin-iMX8MM_ToradexEasyInstaller_${TDX_VER_PACKAGE}"

recipes-bsp/u-boot/u-boot-toradex_2016.11.bbappend
@@ -2,7 +2,7 @@ DEPENDS_append_apalis-tk1 = " cbootimage-native"
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
 PV = "2016.11"
-PR = "${TDX_VER_ITEM}+gitr${SRCPV}"
+PR = "${TDX_RELEASE}+gitr${SRCPV}"
 
 LIC_FILES_CHKSUM = "file://Licenses/README;md5=a2c678cfd4a4d97135585cad908541c6"
 

recipes-tezi/images/tezi-initramfs.bb
@@ -2,7 +2,7 @@ SUMMARY = "Toradex Easy Installer"
 DESCRIPTION = "Toradex Easy Installer for ${MACHINE} machine"
 LICENSE = "MIT"
 
-PV = "${TDX_VER_PACKAGE_MIN}"
+PV = "${TDX_VERSION}"
 
 IMAGE_FSTYPES = "${INITRAMFS_FSTYPES}"
 IMAGE_FSTYPES_remove ="teziimg"

recipes-tezi/qt-tezi/qt-tezi_git.bb
@@ -15,11 +15,11 @@ SRCREV_use-head-next = "${AUTOREV}"
 SRCBRANCH_use-head-next = "master"
 LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=81f0d32e0eab9775391c3bdeb681aadb"
 
-PV = "${TDX_VER_PACKAGE_MIN}+git${SRCPV}"
+PV = "${TDX_VERSION}+git${SRCPV}"
 
 S = "${WORKDIR}/git"
 
-EXTRA_QMAKEVARS_PRE_append = " DEFINES+=VERSION_NUMBER=\\\\\\\"${TDX_VER_PACKAGE_MIN}\\\\\\\""
+EXTRA_QMAKEVARS_PRE_append = " DEFINES+=VERSION_NUMBER=\\\\\\\"${TDX_VERSION}\\\\\\\""
 
 QMAKE_PROFILES = "${S}/tezi.pro"
 

recipes-tezi/qt-tezi/qt-tezictl_git.bb
@@ -15,9 +15,9 @@ LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=1611c5652037dacc6ac585f2443a1979"
 
 S = "${WORKDIR}/git"
 
-PV = "${TDX_VER_PACKAGE_MIN}+git${SRCPV}"
+PV = "${TDX_VERSION}+git${SRCPV}"
 
-EXTRA_QMAKEVARS_PRE_append = " DEFINES+=VERSION_NUMBER=\\\\\\\"${TDX_VER_PACKAGE_MIN}\\\\\\\""
+EXTRA_QMAKEVARS_PRE_append = " DEFINES+=VERSION_NUMBER=\\\\\\\"${TDX_VERSION}\\\\\\\""
 
 inherit qmake5
 

commit db6a4f64bc134f2708fcc57d4d5596c027332c07

image_type_tezi_run: fix vardepsexclude task name typo
do_image_teziimg does not exist. The vardepsexclude flag was silently
ignored, leaving DISTRO_VERSION, TDX_VER_ID and DATE in the basehash
of do_image_tezirunimg. When DATE changes at midnight during a build,
the basehash differs between parse passes causing:

  ERROR: When reparsing ...do_image_tezirunimg, the basehash value changed

Confirmed via bitbake -Sprintdiff which explicitly reported:
  Variable DATE value changed from '20260428' to '20260429'

Rename do_image_teziimg[vardepsexclude] to do_image_tezirunimg[vardepsexclude].

Related-to: AUT-3192
Fixes: 8afcde346e3e ("versioning: change to use latest toradex versioning scheme")
Signed-off-by: David Arnold <david.arnold@toradex.com>

classes/image_type_tezi_run.bbclass
@@ -269,4 +269,4 @@ do_image_tezirunimg[dirs] += "${WORKDIR}/${TDX_VER_ID} ${WORKDIR}"
 do_image_tezirunimg[cleandirs] += "${WORKDIR}/${TDX_VER_ID}"
 do_image_tezirunimg[prefuncs] += "rootfs_tezirun_run_json"
 do_image_tezirunimg[depends] += "virtual/bootloader:do_deploy u-boot-distro-boot:do_deploy virtual/kernel:do_deploy tezi-run-metadata:do_deploy uuu-bin:do_deploy"
-do_image_teziimg[vardepsexclude] = "DISTRO_VERSION TDX_VER_ID DATE"
+do_image_tezirunimg[vardepsexclude] = "DISTRO_VERSION TDX_VER_ID DATE"