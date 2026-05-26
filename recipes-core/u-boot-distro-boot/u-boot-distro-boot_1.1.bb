toradex/meta-toradex-tezi

commit 8d34b1de07c1bbef3dc860bf3ec2473d8eac21bb

u-boot-distro-boot: make all boot.cmd transformations in one pass
This is a small refactoring converting a sequence of sed invocations
into a single one.

Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>
(cherry picked from commit 72e11f904347c1a5efceb2fc35a6d19696e86ccd)

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -64,12 +64,13 @@ BOOT_CMD_FILE = "boot.cmd.in"
 BOOT_CMD_FILE:colibri-imx6ull = "boot.cmd.in-NAND"
 BOOT_CMD_FILE:colibri-imx7 = "boot.cmd.in-NAND"
 
-do_deploy () {
-    sed 's/@@INITRAMFS_FSTYPES@@/${INITRAMFS_FSTYPES}/' ${S}/${BOOT_CMD_FILE} > boot.cmd
-    sed -i 's/@@TEZI_BOOT_ARGS@@/${TEZI_BOOT_ARGS}/' boot.cmd
-    sed -i 's/@@TEZI_FITIMAGE_ADDR@@/${TEZI_FITIMAGE_ADDR}/' boot.cmd
-    sed -i 's/@@TEZI_OVERLAY_ADDR@@/${TEZI_OVERLAY_ADDR}/' boot.cmd
-    sed -i 's/@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@/${TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT}/' boot.cmd
+do_deploy() {
+    sed -e 's/@@INITRAMFS_FSTYPES@@/${INITRAMFS_FSTYPES}/' \
+        -e 's/@@TEZI_BOOT_ARGS@@/${TEZI_BOOT_ARGS}/' \
+        -e 's/@@TEZI_FITIMAGE_ADDR@@/${TEZI_FITIMAGE_ADDR}/' \
+        -e 's/@@TEZI_OVERLAY_ADDR@@/${TEZI_OVERLAY_ADDR}/' \
+        -e 's/@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@/${TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT}/' \
+        "${WORKDIR}/${BOOT_CMD_FILE}" > boot.cmd
 
     uboot-mkimage -T script -C none -a 0 -e 0 \
         -n "TEZI distro boot script" -d boot.cmd ${DEPLOYDIR}/boot.scr-${MACHINE}-${PV}-${PR}

commit e9a429e8dcb91a356a28cb47f4a4478b2a84b6b6

u-boot-distro-boot: Fix boot.cmd.in location
During the forward port of commit 8d34b1de07c1
("u-boot-distro-boot: make all boot.cmd transformations in one pass"),
we forgot to update a reference from ${WORKDIR} to ${S}, leading to the
file boot.cmd.in not being found. Fix that.

This fixes the following error:
  | sed: can't read [...]/u-boot-distro-boot/1.1/boot.cmd.in: No such file or directory
  | WARNING: exit code 2 from a shell command.
  NOTE: recipe u-boot-distro-boot-1.1-r0: task do_compile: Failed
  ERROR: Task (/workdir/oe/build/../layers/meta-toradex-tezi/recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb:do_compile) failed with exit code '1'

Fixes: 8d34b1de07c1 ("u-boot-distro-boot: make all boot.cmd transformations in one pass")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -73,7 +73,7 @@ do_compile() {
         -e 's/@@TEZI_OVERLAY_ADDR@@/${TEZI_OVERLAY_ADDR}/' \
         -e 's/@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@/${TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT}/' \
         -e 's/@@FITCONF_FDT_OVERLAYS@@/${FITCONF_FDT_OVERLAYS}/' \
-        "${WORKDIR}/${BOOT_CMD_FILE}" > boot.cmd
+        "${S}/${BOOT_CMD_FILE}" > boot.cmd
 }
 
 do_deploy() {