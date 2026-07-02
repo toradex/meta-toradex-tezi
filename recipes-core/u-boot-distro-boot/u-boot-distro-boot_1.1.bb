toradex/meta-toradex-tezi

commit a82cf549f708aed5a150ca5f78cb7c9211377c85

u-boot-distro-boot: drop KERNEL_DTB_PREFIX
Master no longer has a prefix for the configuration nodes in a
fitimage. Remove the @@KERNEL_DTB_PREFIX@@ placeholder in the
boot.cmd.in file.

Otherwise the placeholder is not replaced and the resulting boot.cmd
contains an not existing configuration to load and the resulting TEZI
does not boot.

Fixes: commit e2fed1009a3b ("u-boot-distro-boot: unify tezi boot scripts")
Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-core/u-boot-distro-boot/files/boot.cmd.in
@@ -21,7 +21,7 @@ if test "${_res}" = "" -o "${_res}" = "${console}"; then
 fi
 
 env set bootcmd_args 'env set bootargs @@TEZI_BOOT_ARGS@@ console=${console} quiet rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'

recipes-core/u-boot-distro-boot/files/boot.cmd.in-NAND
@@ -17,7 +17,7 @@ test -n ${overlays_file} || env set overlays_file "overlays.txt"
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
 env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
 
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
 
 test -n ${devtype} || env set devtype mmc
 test -n ${devnum} || env set devnum 0

commit 1785de6dd9bbe342530a77a4f94a333a2d0c06c0

u-boot-distro-boot: prefix FIT config name with KERNEL_DTB_PREFIX
Commit 4297b94c3728 ("kernel-fit-image.bbclass: Fix operation with KERNEL_DTBVENDORED = "1"")
from openembedded-core embeds the vendor DTB path into the FIT
configuration node name whenever KERNEL_DTBVENDORED = "1", fixing a
regression that happened during migration of classes in OE 6.0.

We previously dealt with said regression by mirroring its behavior,
dropping KERNEL_DTB_PREFIX. Now, we need to revert thoses changes and
prepend KERNEL_DTB_PREFIX again in our boot commands, and rely on the
proper fix.
Since tdx-base.inc sets KERNEL_DTBVENDORED = "1" globally, every
fitImage we build is affected.

The TEZI boot script hardcodes "#conf-${fdtfile}" with no vendor prefix,
so U-Boot can no longer find the requested configuration node and the
installer fails to boot:

  Loading kernel (any) from FIT Image at 90400000 ...
  Could not find configuration node
  ERROR -2: can't get kernel image!

Match kernel-fit-image.bbclass by using the vendor prefix from
KERNEL_DTB_PREFIX only when KERNEL_DTBVENDORED = "1".

Fixes: a82cf549f708 ("u-boot-distro-boot: drop KERNEL_DTB_PREFIX")
Fixes: e0453742933c ("tezi.itb: follow oe fitimage changes")
Signed-off-by: Antoine Gouby <antoine.gouby@toradex.com>

recipes-core/u-boot-distro-boot/files/boot.cmd.in
@@ -21,7 +21,7 @@ if test "${_res}" = "" -o "${_res}" = "${console}"; then
 fi
 
 env set bootcmd_args 'env set bootargs @@TEZI_BOOT_ARGS@@ console=${console} quiet rootfstype=@@INITRAMFS_FSTYPES@@ root=/dev/ram autoinstall ${teziargs}'
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
 env set tezi_image ${prefix}tezi.itb
 
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'

recipes-core/u-boot-distro-boot/files/boot.cmd.in-NAND
@@ -17,7 +17,7 @@ test -n "${overlays_file}" || env set overlays_file "overlays.txt"
 env set set_default_overlays 'env set fdt_overlays "@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@"'
 env set set_apply_overlays 'env set apply_overlays "for overlay_file in \"\\${fdt_overlays}\"; do env set fitconf_fdt_overlays \"\\"\\${fitconf_fdt_overlays}#conf-\\${overlay_file}\\"\"; env set overlay_file; done; true"'
 
-env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-\${fdtfile}\${fitconf_fdt_overlays}'
+env set bootcmd_run 'echo "Bootargs: \${bootargs}" && bootm ${fitimage_addr}#conf-@@KERNEL_DTB_PREFIX@@\${fdtfile}\${fitconf_fdt_overlays}'
 
 test -n "${devtype}" || env set devtype mmc
 test -n "${devnum}" || env set devnum 0

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -67,6 +67,10 @@ BOOT_CMD_FILE:colibri-imx7 = "boot.cmd.in-NAND"
 
 FITCONF_FDT_OVERLAYS ??= ""
 
+# Mirror the config-node prefix kernel-fit-image.bbclass adds when
+# KERNEL_DTBVENDORED = "1", so the requested FIT config name matches.
+DTB_PREFIX ??= "${@d.getVar('KERNEL_DTB_PREFIX').replace("/", "_") if d.getVar('KERNEL_DTB_PREFIX') and d.getVar('KERNEL_DTBVENDORED') == '1' else ''}"
+
 do_compile() {
     sed -e 's/@@INITRAMFS_FSTYPES@@/${INITRAMFS_FSTYPES}/' \
         -e 's/@@TEZI_BOOT_ARGS@@/${TEZI_BOOT_ARGS}/' \
@@ -74,6 +78,7 @@ do_compile() {
         -e 's/@@TEZI_OVERLAY_ADDR@@/${TEZI_OVERLAY_ADDR}/' \
         -e 's/@@TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT@@/${TEZI_EXTERNAL_KERNEL_DEVICETREE_BOOT}/' \
         -e 's/@@FITCONF_FDT_OVERLAYS@@/${FITCONF_FDT_OVERLAYS}/' \
+        -e 's/@@KERNEL_DTB_PREFIX@@/${DTB_PREFIX}/' \
         "${S}/${BOOT_CMD_FILE}" > boot.cmd
 }
 