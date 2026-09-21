toradex/meta-toradex-tezi

commit 778a58f3de50420bc950d925fdb69b222b1e4937

recipes-bsp/core: support tezi recovery boot for toradex-osm-imx93
Add the parts to boot Tezi on the OSM-iMX93 via USB recovery.
As SPL_BOOTROM_SUPPORT is enabled in U-Boot, the binary can be loaded
via SDPS. The addresses are aligned to the ones specified for the
OSM-iMX93 module in the U-Boot env file.

Related-to: ELB-6721
Signed-off-by: Franz Schnyder <franz.schnyder@toradex.com>

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -20,6 +20,7 @@ TEZI_BOOT_ARGS:colibri-imx6 = "video=DPI-1:640x480-16@60D video=HDMI-A-1:640x480
 TEZI_BOOT_ARGS:colibri-imx6ull = "video=DPI-1:640x480@60D"
 TEZI_BOOT_ARGS:colibri-imx7 = "video=DPI-1:640x480@60D"
 TEZI_BOOT_ARGS:colibri-imx8x = "video=DPI-1:640x480-16@60D"
+TEZI_BOOT_ARGS:toradex-osm-imx93 = "video=HDMI-A-1:1280x720-16@60D"
 TEZI_BOOT_ARGS:toradex-smarc-imx8mp = "video=HDMI-A-1:1280x720-16@60D"
 TEZI_BOOT_ARGS:toradex-smarc-imx95 = "video=DP-1:1280x720@60D"
 TEZI_BOOT_ARGS:verdin-am62 = "video=HDMI-A-1:640x480-16@60D"
@@ -43,6 +44,7 @@ TEZI_FITIMAGE_ADDR:colibri-imx6ull = "0x81100000"
 TEZI_FITIMAGE_ADDR:colibri-imx7 = "0x81100000"
 TEZI_FITIMAGE_ADDR:colibri-imx8x = "0x8a000000"
 TEZI_FITIMAGE_ADDR:mx8mp-generic-bsp = "0x44200000"
+TEZI_FITIMAGE_ADDR:mx93-generic-bsp = "0x82800000"
 TEZI_FITIMAGE_ADDR:mx95-generic-bsp = "0x90400000"
 TEZI_FITIMAGE_ADDR:verdin-imx8mm = "0x44200000"
 
@@ -53,6 +55,7 @@ TEZI_OVERLAY_ADDR:colibri-imx6ull = "0x80f00000"
 TEZI_OVERLAY_ADDR:colibri-imx7 = "0x80f00000"
 TEZI_OVERLAY_ADDR:colibri-imx8x = "0x82e10000"
 TEZI_OVERLAY_ADDR:mx8mp-generic-bsp = "0x42e10000"
+TEZI_OVERLAY_ADDR:mx93-generic-bsp = "0x9c800000"
 TEZI_OVERLAY_ADDR:mx95-generic-bsp = "0x9c800000"
 TEZI_OVERLAY_ADDR:verdin-imx8mm = "0x42e10000"
 

recipes-bsp/tezi-run-metadata/files/toradex-osm-imx93/recovery/uuu.auto
@@ -1,0 +1,11 @@
+uuu_version 1.5.165
+
+SDPS: boot -f ../imx-boot-recoverytezi
+CFG: FB: -vid 0x1b67 -pid 0x4000
+FB: download -f ../boot.scr
+FB: ucmd cp.b 0x82800000 0x9c600000 0x100000
+FB: download -f ../overlays.txt
+FB: ucmd cp.b 0x82800000 0x9c800000 0x100000
+FB: download -f ../tezi.itb
+FB: acmd source 0x9c600000
+FB: done

recipes-bsp/u-boot/u-boot-toradex-tezi.inc
@@ -33,6 +33,10 @@ do_configure:prepend:verdin-imx95 () {
     kconfig_configure_variable BOOTCOMMAND '"fastboot usb 0"' $recoveryconf
 }
 
+do_configure:prepend:toradex-osm-imx93 () {
+    kconfig_configure_variable BOOTCOMMAND '"fastboot usb 0"' $recoveryconf
+}
+
 do_configure:prepend:toradex-smarc-imx8mp () {
     kconfig_configure_variable BOOTCOMMAND '"fastboot usb 0"' $recoveryconf
 }

commit 0dcebcac5b6a93f57e693c9c58a3e0014cfd6c68

u-boot-distro-boot: Move TEZI_OVERLAY_ADDR lower for iMX93 SoMs
TEZI_OVERLAY_ADDR:mx93-generic-bsp is placed at 0x9c800000, the same
value as for the iMX95 SoMs. This can be a problem on iMX93 SoM
configurations with less memory.

In the uuu recovery scripts, we already use 0x8a900000 as our
overlays.txt buffer. The TEZI fit image can place its overlays.txt at
the same address.

In this case, 0x8a900000 is ramdisk_addr_r, and is also
UBOOT_RD_LOADADDRESS. TEZI imports overlays.txt into the U-Boot
environment and builds the FIT configuration selection. Therefore, by
the time the ramdisk is loaded from the FIT image, it is fine to
overwrite the overlays.txt buffer. The recovery path does the same.

Related-to: ELB-7272
Fixes: 778a58f3de50 ("recipes-bsp/core: support tezi recovery boot for toradex-osm-imx93")
Signed-off-by: Ernest Van Hoecke <ernest.vanhoecke@toradex.com>

recipes-core/u-boot-distro-boot/u-boot-distro-boot_1.1.bb
@@ -57,7 +57,7 @@ TEZI_OVERLAY_ADDR:colibri-imx6ull = "0x80f00000"
 TEZI_OVERLAY_ADDR:colibri-imx7 = "0x80f00000"
 TEZI_OVERLAY_ADDR:colibri-imx8x = "0x82e10000"
 TEZI_OVERLAY_ADDR:mx8mp-generic-bsp = "0x42e10000"
-TEZI_OVERLAY_ADDR:mx93-generic-bsp = "0x9c800000"
+TEZI_OVERLAY_ADDR:mx93-generic-bsp = "0x8a900000"
 TEZI_OVERLAY_ADDR:mx95-generic-bsp = "0x9c800000"
 TEZI_OVERLAY_ADDR:verdin-imx8mm = "0x42e10000"
 